957cf69b80db10062ee509a4b061b537a6d4ff1f
hak0622 <128469147+hak0622@users.noreply.github.com>
2025-03-31 09:50:56 +0900

    [level 3] Title: 물고기 종류 별 대어 찾기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/3/293261. 물고기 종류 별 대어 찾기/물고기 종류 별 대어 찾기.sql
@@ -0,0 +1,6 @@
+SELECT a.ID,b.FISH_NAME,a.LENGTH
+FROM FISH_INFO a,FISH_NAME_INFO b
+WHERE a.FISH_TYPE = b.FISH_TYPE AND
+            (a.FISH_TYPE,a.LENGTH) IN (SELECT FISH_TYPE,MAX(LENGTH)
+                          FROM FISH_INFO
+                          GROUP BY FISH_TYPE);
