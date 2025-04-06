fc616ebe9d389327ceb8b6b6a446e57185457700
hak0622 <128469147+hak0622@users.noreply.github.com>
2025-04-06 20:49:21 +0900

    [level 0] Title: 두 수의 차 구하기, Time: 0.04 ms, Memory: 89.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120803. 두 수의 차 구하기/두 수의 차 구하기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int num1, int num2) {
+        int answer = 0;
+        if(num1<num2){
+            answer=num1-num2;
+        }else{
+         answer=num1-num2;
+        }
+        return answer;
+    }
+}
