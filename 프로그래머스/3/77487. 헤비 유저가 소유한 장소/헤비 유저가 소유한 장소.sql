631f15de20f07463bb62deb75b9520afd3728886
hak0622 <128469147+hak0622@users.noreply.github.com>
2025-04-21 13:56:30 +0900

    [level 3] Title: 헤비 유저가 소유한 장소, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/77487. 헤비 유저가 소유한 장소/헤비 유저가 소유한 장소.sql
@@ -0,0 +1,7 @@
+select ID,NAME,HOST_ID
+from PLACES
+where HOST_ID IN (select HOST_ID
+                 from PLACES
+                 group by HOST_ID
+                 having count(*)>=2)
+order by ID
