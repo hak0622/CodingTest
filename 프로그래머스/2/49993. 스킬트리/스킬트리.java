1d29433840ee70804f4a1438b07bae5d90e7fdf0
hak0622 <128469147+hak0622@users.noreply.github.com>
2025-05-10 16:35:54 +0900

    [level 2] Title: 스킬트리, Time: 1.13 ms, Memory: 69.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int solution(String skill, String[] skill_trees) {
+        int answer = 0;
+
+		for (int i = 0; i < skill_trees.length; i++) {
+			String[] s = skill_trees[i].split("");
+			int index = 0;
+			boolean ck = true;
+
+			for (int j = 0; j < s.length; j++) {
+				if (index < skill.indexOf(s[j])) {
+					ck = false;
+					break;
+				} else if (index == skill.indexOf(s[j])) {
+					index++;
+				}
+			}
+
+			if (ck) {
+				answer++;
+			}
+		}
+		return answer;
+    }
+}
