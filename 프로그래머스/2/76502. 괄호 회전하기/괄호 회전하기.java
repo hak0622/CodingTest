9f7acff7dc18fc07969cb95896f3d00a7d232f22
hak0622 <128469147+hak0622@users.noreply.github.com>
2025-05-29 15:41:20 +0900

    [level 2] Title: 괄호 회전하기, Time: 29.59 ms, Memory: 96.1 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -1,32 +1,32 @@
 import java.util.*;
 class Solution {
-    boolean isValid (String s) {
-        Stack<Character> st = new Stack<>();
+    public int solution(String s) {
+        int answer = 0;
+        String doubleS = s+s;
+        for(int i=0; i<s.length(); i++){
+            if(isValid(doubleS.substring(i,i+s.length()))){
+                answer++;
+            }
+        }
+        return answer;
+    }
+    boolean isValid(String s){
+        Stack<Character>st = new Stack<>();
         for(char current : s.toCharArray()){
-            if(current == '(' || current == '{' || current == '['){
+            if(current == '('|| current=='{'||current=='['){
                 st.push(current);
             }else{
                 if(st.isEmpty()){
                     return false;
                 }
                 char target = st.pop();
-                if((target == '(' && current !=')')||
-                   (target == '{' && current !='}')||
-                   (target == '[' && current !=']')){
+                if(target == '(' && current != ')'||
+                   target == '{' && current != '}'||
+                   target == '[' && current != ']'){
                     return false;
                 }
             }
         }
         return st.isEmpty();
-    }
-    public int solution(String s){
-        String doubleS = s+s;
-        int answer = 0;
-        for(int i=0; i<s.length(); i++){
-            if(isValid(doubleS.substring(i,i+ s.length()))){
-                answer++;
-            }
-        }
-        return answer;
     }
 }
