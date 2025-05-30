dbe985036f9916ed967f1482d30d9df984859a86
hak0622 <128469147+hak0622@users.noreply.github.com>
2025-05-30 09:39:43 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 14.52 ms, Memory: 75.2 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,29 +1,28 @@
-import java.util.*; 
+import java.util.*;
 
 class Solution {
     public int solution(int n, int[][] wires) {
-        List<List<Integer>>graph = new ArrayList<>();
         int answer = Integer.MAX_VALUE;
+        List<List<Integer>>graph = new ArrayList<>();
         
         for(int i=0; i<n; i++){
             graph.add(i,new ArrayList<>());
         }
         for(int[]edge : wires){
-            int a = edge[0] - 1;
-            int b = edge[1] - 1;
+            int a = edge[0]-1;
+            int b = edge[1]-1;
             
             graph.get(a).add(b);
             graph.get(b).add(a);
         }
         for(int[]edge : wires){
-            int a = edge[0] - 1;
-            int b = edge[1] - 1;
+            int a = edge[0]-1;
+            int b = edge[1]-1;
             
             graph.get(a).remove(Integer.valueOf(b));
             graph.get(b).remove(Integer.valueOf(a));
             
             int count = bfs(a,graph,n);
-            
             int diff = Math.abs(count - (n - count));
             answer = Math.min(answer,diff);
             
@@ -32,23 +31,23 @@ class Solution {
         }
         return answer;
     }
-    public int bfs(int startVertex,List<List<Integer>>graph,int n){
+    public int bfs(int start,List<List<Integer>>graph,int n){
         boolean[]visited = new boolean[n];
-        Queue<Integer> q = new LinkedList<>();
-        q.add(startVertex);
-        visited[startVertex] = true;
-        int count = 1;
+        Queue<Integer>q = new LinkedList<>();
+        q.add(start);
+        visited[start]=true;
+        int cnt = 1;
         
         while(!q.isEmpty()){
-            int curVertex = q.poll();
-            for(int nextVertex : graph.get(curVertex)){
-                if(!visited[nextVertex]){
-                    q.add(nextVertex);
-                    visited[nextVertex] = true;
-                    count++;
+            int current = q.poll();
+            for(int next : graph.get(current)){
+                if(!visited[next]){
+                    q.add(next);
+                    visited[next]=true;
+                    cnt++;
                 }
             }
         }
-        return count;
+        return cnt;
     }
 }
