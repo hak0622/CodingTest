2539ec6ad813f81580aeeb8c671b78f8b6c40c14
hak0622 <128469147+hak0622@users.noreply.github.com>
2025-06-09 17:43:54 +0900

    [level 0] Title: 뒤집힌 문자열, Time: 0.05 ms, Memory: 81.5 MB -BaekjoonHub

--- 프로그래머스/0/120822. 뒤집힌 문자열/뒤집힌 문자열.java
+++ 프로그래머스/0/120822. 뒤집힌 문자열/뒤집힌 문자열.java
@@ -2,17 +2,6 @@ import java.util.*;
 
 class Solution {
     public String solution(String my_string) {
-        
-        char[]arr = new char[my_string.length()];
-        char[]reverse = new char[my_string.length()];
-        
-        for(int i=0; i<my_string.length(); i++){
-            arr[i] = my_string.charAt(i);
-        }
-        for(int j=arr.length-1,k=0; j>=0; j--,k++){
-           reverse[k]=arr[j];
-        }
-        
-        return new String(reverse);
+        return new StringBuilder(my_string).reverse().toString();
     }
 }
