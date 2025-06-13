266eb70c0d4026a818b2939e5fc5c3ede4c961fd
hak0622 <128469147+hak0622@users.noreply.github.com>
2025-06-13 16:26:26 +0900

    [level 0] Title: 약수 구하기, Time: 0.53 ms, Memory: 68.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] solution(int n) {
+        int count = 0;
+        int index = 0;
+        for(int i=1; i<=n; i++){
+            if(n%i==0){
+                count++;
+            }
+        }
+        int[] answer = new int[count];
+       
+        for(int i=1; i<=n; i++){
+            if(n%i==0){
+                answer[index++] = i;
+            }
+        }
+        return answer;
+    }
+}
