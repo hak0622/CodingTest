6f92007f58e6780f838f3565c078ea9a3d5eec80
hak0622 <128469147+hak0622@users.noreply.github.com>
2025-06-13 14:26:13 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.02 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
@@ -1,15 +1,14 @@
 class Solution {
     public int solution(int price) {
-        int answer = 0;
         if(price >= 500000){
-            answer = (int)(price * 0.8);
-        }else if(price >= 300000){
-            answer = (int)(price * 0.9);
-        }else if(price >= 100000){
-             answer = (int)(price * 0.95);  
-        }else{
-            answer = price;
+            return (int)(price*0.8);
         }
-        return answer;
+        else if(price >= 300000){
+            return (int)(price*0.9);
+        }
+        else if(price >= 100000){
+            return (int)(price*0.95);
+        }
+        return price;
     }
 }
