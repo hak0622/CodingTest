e515726ac5e5a2580b233933b57852164e3309ba
hak0622 <128469147+hak0622@users.noreply.github.com>
2025-06-16 14:01:26 +0900

    [level 0] Title: 수 조작하기 2, Time: 1115.03 ms, Memory: 392 MB -BaekjoonHub

---
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String solution(int[] numLog) {
+        String answer = "";
+        for(int i=0; i<numLog.length-1; i++){
+            if(numLog[i+1] - numLog[i] == 1){
+                answer = answer + 'w';
+            }else if(numLog[i+1] - numLog[i] == -1){
+                answer = answer + 's';
+            }else if(numLog[i+1] - numLog[i] == 10){
+                answer = answer + 'd';
+            }else{
+                answer = answer + 'a';
+            }
+        }
+        return answer;
+    }
+}
