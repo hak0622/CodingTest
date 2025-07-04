06c1673df28146954d2e02a752c89b06c59e88d1
hak0622 <128469147+hak0622@users.noreply.github.com>
2025-07-04 11:31:17 +0900

    [level 3] Title: 합승 택시 요금, Time: 80.77 ms, Memory: 64.7 MB -BaekjoonHub

--- 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.java
+++ 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.java
@@ -6,26 +6,24 @@ class Solution {
         for(int i=1; i<=n; i++){
             Arrays.fill(map[i],200*100000);
             map[i][i] = 0;
-        } 
-        
-        for(int[] fare : fares){
+        }
+        for(int[]fare : fares){
             map[fare[0]][fare[1]] = fare[2];
             map[fare[1]][fare[0]] = fare[2];
         }
         
-        for(int k=1; k <=n; k++){
+        for(int k=1; k<=n; k++){
             for(int i=1; i<=n; i++){
                 for(int j=1; j<=n; j++){
-                    if(map[i][j] > map[i][k] + map[k][j]){
-                        map[i][j] = map[i][k] + map[k][j];
+                    if(map[i][j] > map[i][k]+map[k][j]){
+                        map[i][j] = map[i][k]+map[k][j];
                     }
                 }
             }
         }
         int answer = map[s][a] + map[s][b];
-        
         for(int i=1; i<=n; i++){
-            answer = Math.min(answer, map[s][i] + map[i][a] + map[i][b]);
+            answer = Math.min(answer,map[s][i] + map[i][a] + map[i][b]);
         }
         return answer;
     }
