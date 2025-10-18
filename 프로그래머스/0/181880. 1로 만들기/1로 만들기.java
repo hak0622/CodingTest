eaa822c8b30d19be85d9e87cf9ba1463c4b78c92
hak0622 <128469147+hak0622@users.noreply.github.com>
2025-10-18 12:55:32 +0900

    [level 0] Title: 1로 만들기, Time: 0.05 ms, Memory: 75.4 MB -BaekjoonHub

--- 프로그래머스/0/181880. 1로 만들기/1로 만들기.java
+++ 프로그래머스/0/181880. 1로 만들기/1로 만들기.java
@@ -1,12 +1,12 @@
 class Solution {
     public int solution(int[] num_list) {
         int answer = 0;
-        for(int n : num_list){
-            while(n>1){
-                if(n%2 ==0){
-                    n = n/2;
+        for(int num : num_list){
+            while(num>1){
+                if(num%2==0){
+                    num = num/2;
                 }else{
-                    n = (n-1)/2;
+                    num = (num-1)/2;
                 }
                 answer++;
             }
