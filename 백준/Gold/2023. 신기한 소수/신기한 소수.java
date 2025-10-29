e1f06aca8fee20495b654942aa94fbfce1be8eb6
hak0622 <128469147+hak0622@users.noreply.github.com>
2025-10-29 16:49:06 +0900

    [Gold V] Title: 신기한 소수, Time: 692 ms, Memory: 14340 KB -BaekjoonHub

---
+++ 백준/Gold/2023. 신기한 소수/신기한 소수.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+
+        DFS(2,1);
+        DFS(3,1);
+        DFS(5,1);
+        DFS(7,1);
+    }
+
+    public static void DFS(int number, int jarisu){
+        if(jarisu == N){
+            if(isPrime(number)){
+                System.out.println(number);
+            }
+            return;
+        }
+
+        for(int i=1; i<10; i++){
+            if(i%2 ==0){
+                continue;
+            }
+            if(isPrime(number*10+i)){
+                DFS(number * 10 + i,jarisu+1);
+            }
+        }
+    }
+
+    public static boolean isPrime(int num){
+        for(int i=2; i<=num/2; i++){
+            if(num % i==0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
