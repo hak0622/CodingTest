f27cc533be7cca7e05b82bd4615592a3e2e89ddc
hak0622 <128469147+hak0622@users.noreply.github.com>
2025-11-07 22:48:08 +0900

    [level 0] Title: 리스트 자르기, Time: 2.12 ms, Memory: 74.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int n, int[] slicer, int[] num_list) {
+        int[] answer = {};
+        List<Integer>list = new ArrayList<>();
+        
+        int a = slicer[0];
+        int b = slicer[1];
+        int c = slicer[2];
+        
+        switch(n){
+            case 1:
+                answer = Arrays.copyOfRange(num_list,0,b+1);
+                break;
+            case 2:
+                answer = Arrays.copyOfRange(num_list,a,num_list.length);
+                break;
+            case 3:
+                answer = Arrays.copyOfRange(num_list,a,b+1);
+                break;
+            case 4:  
+                for(int i=a; i<=b; i+=c){
+                    list.add(num_list[i]);
+                }
+                answer = list.stream().mapToInt(i -> i).toArray();
+                break;
+        }
+        return answer;
+    }
+}
