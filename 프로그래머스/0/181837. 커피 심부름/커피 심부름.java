28aee24560c80952794e7b309733971b84d68c7c
hak0622 <128469147+hak0622@users.noreply.github.com>
2025-11-08 15:40:02 +0900

    [level 0] Title: 커피 심부름, Time: 0.79 ms, Memory: 86.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181837. 커피 심부름/커피 심부름.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(String[] order) {
+        int answer = 0;
+        
+        for(int i=0; i < order.length; i++){
+            if(order[i].contains("americano") || order[i].contains("anything")){
+                answer = answer + 4500;
+            }else if(order[i].contains("cafelatte")){
+                answer = answer + 5000;
+            }
+        }
+        
+        return answer;
+    }
+}
