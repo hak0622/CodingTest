b6d0d7ee82707a83a7b6df7c1d447eba578b7ca9
hak0622 <128469147+hak0622@users.noreply.github.com>
2025-11-09 20:42:43 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 0.07 ms, Memory: 87.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
@@ -0,0 +1,7 @@
+class Solution {
+    public String solution(String my_string, String overwrite_string, int s) {
+        StringBuilder answer = new StringBuilder(my_string);
+        answer.replace(s,s + overwrite_string.length(),overwrite_string);
+        return answer.toString();
+    }
+}
