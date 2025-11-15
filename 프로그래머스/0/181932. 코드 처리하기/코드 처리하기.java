4c8ccfff620783184e4373398206364ec143419b
hak0622 <128469147+hak0622@users.noreply.github.com>
2025-11-15 15:31:07 +0900

    [level 0] Title: 코드 처리하기, Time: 10.31 ms, Memory: 85.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String solution(String code) {
+        StringBuilder sb = new StringBuilder();
+        int mode = 0;
+        
+        for(int i=0; i<code.length(); i++){
+            char c = code.charAt(i);
+            
+            if(c == '1'){
+                mode = 1 - mode;
+                continue;
+            }
+            
+            if(mode == 0){
+                if(i%2==0){
+                    sb.append(c);
+                }
+            }else{
+                if(i%2==1){
+                    sb.append(c);
+                }
+            }
+        }
+        return sb.length() == 0 ? "EMPTY" : sb.toString();
+    }
+}
