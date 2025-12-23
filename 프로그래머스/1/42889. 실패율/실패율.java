6f514690402a0c85af0733e5dd8d62074ed41c2a
hak0622 <128469147+hak0622@users.noreply.github.com>
2025-12-23 14:08:39 +0900

    [level 1] Title: 실패율, Time: 5.11 ms, Memory: 84.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/42889. 실패율/실패율.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int[] solution(int N, int[] stages) {
+        int[]count = new int[N+2];
+        for(int i=0; i<stages.length; i++){
+            count[stages[i]]++;
+        }
+        
+        double[]fail = new double[N];
+        int users = stages.length;
+        
+        for(int i=0; i<N; i++){
+            fail[i] = (double)count[i+1] / users;
+            users = users - count[i+1];
+        }
+        
+        int[]stage = new int[N];
+        for(int i=0; i<N; i++){
+            stage[i] = i+1;
+        }
+        
+        for(int i=0; i<N-1; i++){
+            for(int j=0; j<N-1-i; j++){
+                if(fail[j] < fail[j+1]){
+                    double tempFail = fail[j];
+                    fail[j] = fail[j+1];
+                    fail[j+1] = tempFail;
+                    
+                    int tempStage = stage[j];
+                    stage[j] = stage[j+1];
+                    stage[j+1] = tempStage;
+                }
+            }
+        }
+        return stage;
+    }
+}
