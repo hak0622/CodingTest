6f2e70a4c0c1b4ddf29de3b90bd8b849b45300c8
hak0622 <128469147+hak0622@users.noreply.github.com>
2026-01-04 17:53:00 +0900

    [level 1] Title: 콜라 문제, Time: 0.07 ms, Memory: 78 MB -BaekjoonHub

--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.java
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.java
@@ -1,12 +1,10 @@
 class Solution {
     public int solution(int a, int b, int n) {
         int answer = 0;
-        
         while(n >= a){
             int coke = (n / a) * b;
             answer = answer + coke;
-            
-            n = (n % a) + coke;
+            n = n % a +coke;
         }
         return answer;
     }
