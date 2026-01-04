83772751416ff42467ebf2220eaf7ca1398aa969
hak0622 <128469147+hak0622@users.noreply.github.com>
2026-01-04 17:40:48 +0900

    [level 1] Title: 추억 점수, Time: 2.40 ms, Memory: 112 MB -BaekjoonHub

---
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String[] name, int[] yearning, String[][] photo) {
+        Map<String,Integer> map = new HashMap<>();
+        
+        for(int i=0; i<name.length; i++){
+            map.put(name[i],yearning[i]);
+        }
+        
+        int[]answer = new int[photo.length];
+        
+        for(int i=0; i<photo.length; i++){
+            int sum = 0;
+            
+            for(int j=0; j<photo[i].length; j++){
+                String person = photo[i][j];
+                if(map.containsKey(person)){
+                    sum = sum + map.get(person);
+                }
+            }
+            answer[i] = sum;
+        }
+        return answer;
+    }
+}
