83cd569b944231497016533fb38465372f0d1ee5
hak0622 <128469147+hak0622@users.noreply.github.com>
2026-01-13 17:17:41 +0900

    [level 1] Title: 둘만의 암호, Time: 1.04 ms, Memory: 73.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String solution(String s, String skip, int index) {
+        StringBuilder sb = new StringBuilder();
+        
+        for(int i=0; i<s.length(); i++){
+            char c = s.charAt(i);
+            int count = 0;
+            
+            while(count < index){
+                c++;
+                
+                if(c > 'z'){
+                    c = 'a';
+                }
+                
+                if(skip.contains(String.valueOf(c))){
+                    continue;
+                }
+                count++;
+            }
+            sb.append(c);
+        }
+        return sb.toString();
+    }
+}
