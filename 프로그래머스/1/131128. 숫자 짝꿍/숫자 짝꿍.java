4305bcd0e41dc76e0a7d3068be5637db828f86b4
hak0622 <128469147+hak0622@users.noreply.github.com>
2026-02-01 21:51:15 +0900

    [level 1] Title: 숫자 짝꿍, Time: 58.60 ms, Memory: 127 MB -BaekjoonHub

---
+++ 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String solution(String X, String Y) {
+        int[]countX = new int[10];
+        int[]countY = new int[10];
+        
+        for(int i=0; i<X.length(); i++){
+            countX[X.charAt(i) - '0']++;
+        }
+        for(int i=0; i<Y.length(); i++){
+            countY[Y.charAt(i) - '0']++;
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        for(int d=9; d>=0; d--){
+            int common = Math.min(countX[d],countY[d]);
+            for(int i=0; i<common; i++){
+                sb.append(d);
+            }
+        }
+        if(sb.length() == 0) return "-1";
+        if(sb.charAt(0) == '0') return "0";
+        
+        return sb.toString();
+    }
+}
