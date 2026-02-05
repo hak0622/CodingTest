d1aad314fadfe51dfa420e79c63698cb34b7c27f
hak0622 <128469147+hak0622@users.noreply.github.com>
2026-02-05 22:34:49 +0900

    [level 1] Title: 예산, Time: 0.55 ms, Memory: 97.9 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/예산.java
+++ 프로그래머스/1/12982. 예산/예산.java
@@ -3,17 +3,15 @@ import java.util.*;
 class Solution {
     public int solution(int[] d, int budget) {
         int answer = 0;
-        int sum = 0;
-        
         Arrays.sort(d);
         
         for(int i=0; i<d.length; i++){
-            sum = sum + d[i];
-            if(sum > budget){
-                break;
+            if(d[i] <= budget){
+                budget = budget - d[i];
+                answer++;
             }
-            answer++;
         }
         return answer;
     }
 }
+
