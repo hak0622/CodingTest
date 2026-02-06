6a7ad025d4f925ccb73b0bfe4b21e09711ebbdb8
hak0622 <128469147+hak0622@users.noreply.github.com>
2026-02-06 15:03:30 +0900

    [level 1] Title: K번째수, Time: 0.55 ms, Memory: 72.8 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -5,20 +5,17 @@ class Solution {
         int[]answer = new int[commands.length];
         
         for(int i=0; i<commands.length; i++){
-            int start = commands[i][0] - 1;
-            int end = commands[i][1] - 1;
-            int[]sub = new int[end - start +1];
-            int idx = 0;
+            int[]arr = new int[commands[i][1] - commands[i][0] + 1];
+            int index = 0;
             
-            for(int j=start; j<=end; j++){
-                sub[idx++] = array[j];
+            for(int j=commands[i][0]-1; j<commands[i][1]; j++){
+                arr[index] = array[j];
+                index++;
             }
-            
-            Arrays.sort(sub);
-            
-            answer[i] = sub[commands[i][2] - 1];
+            Arrays.sort(arr);
+            answer[i] = arr[commands[i][2] - 1];
         }
+        
         return answer;
     }
 }
-
