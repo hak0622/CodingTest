6627f5c192fdbc589318037afa0af7397dcbc171
hak0622 <128469147+hak0622@users.noreply.github.com>
2026-02-07 18:43:44 +0900

    [level 1] Title: [1차] 비밀지도, Time: 2.79 ms, Memory: 77.1 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -1,31 +1,19 @@
+import java.util.*;
+
 class Solution {
     public String[] solution(int n, int[] arr1, int[] arr2) {
         String[] answer = new String[n];
-        StringBuilder sb = new StringBuilder();
         
         for(int i=0; i<n; i++){
-            String binary1 = Integer.toBinaryString(arr1[i]);
-            String binary2 = Integer.toBinaryString(arr2[i]);
+            int merged = arr1[i] | arr2[i];
+            String bin = Integer.toBinaryString(merged);
             
-            while(binary1.length() < n){
-                binary1 = "0" + binary1;
-            }
-            while(binary2.length() < n){
-                binary2 = "0" + binary2;
+            while(bin.length() < n){
+                bin = "0" + bin;
             }
             
-            for(int j=0; j<n; j++){
-                char ch1 = binary1.charAt(j);
-                char ch2 = binary2.charAt(j);
-                
-                if(ch1 == '1' || ch2 == '1'){
-                    sb.append("#");
-                }else{
-                    sb.append(" ");
-                }
-            }
-            answer[i] = sb.toString();
-            sb.setLength(0);
+            bin = bin.replace('1','#').replace('0',' ');
+            answer[i] = bin;
         }
         return answer;
     }
