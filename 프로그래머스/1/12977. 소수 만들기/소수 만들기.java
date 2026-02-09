9aee2edb3e9474cc1d472f7e21dcbc97c3bc6cb9
hak0622 <128469147+hak0622@users.noreply.github.com>
2026-02-09 13:33:10 +0900

    [level 1] Title: 소수 만들기, Time: 1.70 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
@@ -2,30 +2,28 @@ class Solution {
     public int solution(int[] nums) {
         int answer = 0;
         
-        for(int i=0; i<nums.length-2; i++){
-            for(int j=i+1; j<nums.length -1; j++){
+        for(int i=0; i<nums.length - 2; i++){
+            for(int j=i+1; j<nums.length - 1; j++){
                 for(int k=j+1; k<nums.length; k++){
                     int sum = nums[i] + nums[j] + nums[k];
                     
-                    boolean isPrime = true;
-                    
-                    if(sum < 2){
-                        isPrime = false;
-                    }else{
-                        for(int d = 2; d*d <=sum; d++){
-                            if(sum % d ==0){
-                                isPrime = false;
-                                break;
-                            }
-                        }
-                    }
-                    
-                    if(isPrime){
+                    if(isPrime(sum)){
                         answer++;
                     }
                 }
             }
         }
         return answer;
+    }
+    
+    private boolean isPrime(int n){
+        if(n < 2) return false;
+        if(n == 2) return true;
+        if(n % 2 ==0) return false;
+        
+        for(int i=3; i*i<=n; i=i+2){
+            if(n % i == 0) return false;
+        }
+        return true;
     }
 }
