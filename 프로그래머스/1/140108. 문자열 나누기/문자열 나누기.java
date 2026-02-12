f7e4a0738fbe35cbb69eca04afeebf85521f14fc
hak0622 <128469147+hak0622@users.noreply.github.com>
2026-02-12 15:01:01 +0900

    [level 1] Title: 문자열 나누기, Time: 0.96 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.java
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.java
@@ -1,6 +1,33 @@
 class Solution {
     public int solution(String s) {
         int answer = 0;
+        int same = 0;
+        int diff = 0;
+        char x = ' ';
+        
+        for(int i=0; i<s.length(); i++){
+            char ch = s.charAt(i);
+            
+            if(same == 0 && diff == 0){
+                x = ch;
+            }
+            
+            if(x == ch){
+                same++;
+            }else{
+                diff++;
+            }
+            
+            if(same == diff){
+                answer++;
+                same = 0;
+                diff = 0;
+            }
+        }
+        
+        if(same != 0 || diff != 0){
+            answer++;
+        }
         return answer;
     }
 }
