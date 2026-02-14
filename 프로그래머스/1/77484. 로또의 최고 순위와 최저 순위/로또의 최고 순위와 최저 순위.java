487e2786073b794975d8468308ac5195e7d6c044
hak0622 <128469147+hak0622@users.noreply.github.com>
2026-02-14 21:33:40 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.02 ms, Memory: 76.9 MB -BaekjoonHub

--- 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
@@ -1,32 +1,39 @@
 class Solution {
     public int[] solution(int[] lottos, int[] win_nums) {
-        int zeroCount = 0;
-        int matchCount = 0;
-
-        for (int myNum : lottos) {
-            if (myNum == 0) {
-                zeroCount++;
-            } else {
-                for (int winNum : win_nums) {
-                    if (myNum == winNum) {
-                        matchCount++;
-                        break; 
-                    }
+        int[]answer = new int[2];
+        int count = 0;
+        int zero = 0;
+        int max = 0;
+        int min = 0;
+        
+        for(int i=0; i<lottos.length; i++){
+            if(lottos[i] == 0){
+                zero++;
+                continue;
+            }
+            for(int j=0; j<win_nums.length; j++){
+                if(lottos[i] == win_nums[j]){
+                    count++;
                 }
             }
         }
-        int maxHits = matchCount + zeroCount;
-        int minHits = matchCount;
-
-        return new int[] {getRank(maxHits), getRank(minHits)};
-    }
-
-    public int getRank(int hits) {
-        if (hits == 6) return 1;
-        if (hits == 5) return 2;
-        if (hits == 4) return 3;
-        if (hits == 3) return 4;
-        if (hits == 2) return 5;
-        return 6; 
+        max = count + zero;
+        min = count;
+        
+        if(max == 6) answer[0] = 1;
+        else if(max == 5) answer[0] = 2;
+        else if(max == 4) answer[0] = 3;
+        else if(max == 3) answer[0] = 4;
+        else if(max == 2) answer[0] = 5;
+        else answer[0] = 6;
+        
+        if(min == 6) answer[1] = 1;
+        else if(min == 5) answer[1] = 2;
+        else if(min == 4) answer[1] = 3;
+        else if(min == 3) answer[1] = 4;
+        else if(min == 2) answer[1] = 5;
+        else answer[1] = 6;
+        
+        return answer;
     }
 }
