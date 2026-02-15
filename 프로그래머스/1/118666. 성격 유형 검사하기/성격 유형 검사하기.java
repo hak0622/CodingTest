56326116120f7708b7433494a2b5eb713d783722
hak0622 <128469147+hak0622@users.noreply.github.com>
2026-02-15 18:05:36 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.75 ms, Memory: 89.5 MB -BaekjoonHub

--- 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.java
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.java
@@ -2,40 +2,29 @@ import java.util.*;
 
 class Solution {
     public String solution(String[] survey, int[] choices) {
-        Map<Character,Integer>score = new HashMap<>();
+        Map<Character,Integer>map = new HashMap<>();
+        char[]types = {'R', 'T', 'C', 'F', 'J', 'M', 'A', 'N'};
         
-        char[]all = {'R','T','C','F','J','M','A','N'};
-        for(char c : all){
-            score.put(c,0);
+        for(char t : types){
+            map.put(t,0);
         }
         
         for(int i=0; i<survey.length; i++){
-            char left = survey[i].charAt(0);
-            char right = survey[i].charAt(1);
             int choice = choices[i];
             
             if(choice < 4){
-                int point = 4 - choice;
-                score.put(left,score.get(left) + point);
+                char type = survey[i].charAt(0);
+                map.put(type, map.get(type) + (4 - choice));
             }else if(choice > 4){
-                int point = choice - 4;
-                score.put(right,score.get(right)+point);
+                char type = survey[i].charAt(1);
+                map.put(type, map.get(type) + (choice - 4));
             }
         }
-        
         StringBuilder sb = new StringBuilder();
-        sb.append(pick(score, 'R','T'));
-        sb.append(pick(score, 'C', 'F'));
-        sb.append(pick(score, 'J', 'M'));
-        sb.append(pick(score, 'A', 'N'));
-        
+        sb.append(map.get('R') >= map.get('T') ? 'R' : 'T');
+        sb.append(map.get('C') >= map.get('F') ? 'C' : 'F');
+        sb.append(map.get('J') >= map.get('M') ? 'J' : 'M');
+        sb.append(map.get('A') >= map.get('N') ? 'A' : 'N');
         return sb.toString();
-    }
-    
-    private char pick(Map<Character,Integer>score, char left, char right){
-        if(score.get(left) >= score.get(right)){
-            return left;
-        }
-        return right;
     }
 }
