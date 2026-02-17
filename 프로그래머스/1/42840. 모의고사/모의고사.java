a72bb49f4cece9945066d368e061f38bfdfc9c67
hak0622 <128469147+hak0622@users.noreply.github.com>
2026-02-17 17:26:21 +0900

    [level 1] Title: 모의고사, Time: 3.32 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -3,29 +3,30 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] answers) {
         int[]supo1 = {1,2,3,4,5};
-        int[]supo2 = {2,1,2,3,2,4,2,5};
-        int[]supo3 = {3,3,1,1,2,2,4,4,5,5};
-        
-        int score1 = 0, score2 = 0, score3 = 0;
+        int[]supo2 = {2, 1, 2, 3, 2, 4, 2, 5};
+        int[]supo3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+        int count1 = 0;
+        int count2 = 0;
+        int count3 = 0;
         
         for(int i=0; i<answers.length; i++){
-            if(answers[i] == supo1[i % supo1.length]) score1++;
-            if(answers[i] == supo2[i % supo2.length]) score2++;
-            if(answers[i] == supo3[i % supo3.length]) score3++;
+            if(answers[i] == supo1[i % supo1.length]){
+                count1++;
+            }
+            if(answers[i] == supo2[i % supo2.length]){
+                count2++;
+            }
+            if(answers[i] == supo3[i % supo3.length]){
+                count3++;
+            }
         }
         
-        int max = Math.max(score1,Math.max(score2,score3));
-        
+        int max = Math.max(count1,Math.max(count2,count3));
         List<Integer>list = new ArrayList<>();
-        if(score1 == max) list.add(1);
-        if(score2 == max) list.add(2);
-        if(score3 == max) list.add(3);
+        if(max == count1) list.add(1);
+        if(max == count2) list.add(2);
+        if(max == count3) list.add(3);
         
-        int[]answer = new int[list.size()];
-        for(int i=0; i<list.size(); i++){
-            answer[i] = list.get(i);
-        }
-        
-        return answer;
+        return list.stream().mapToInt(Integer::intValue).toArray();
     }
 }
