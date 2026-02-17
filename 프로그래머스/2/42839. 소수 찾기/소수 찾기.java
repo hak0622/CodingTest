c82f5d43c31ff4575cffdf60a5317a7c9484e680
hak0622 <128469147+hak0622@users.noreply.github.com>
2026-02-17 17:45:09 +0900

    [level 2] Title: 소수 찾기, Time: 37.09 ms, Memory: 89.2 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,50 +1,35 @@
 import java.util.*;
 
 class Solution {
-    List<Integer>list = new ArrayList<>();
-    boolean[]visited = new boolean[7];
+    HashSet<Integer>numberSet = new HashSet<>();
     
     public int solution(String numbers) {
-        int answer = 0;
-        for(int i=0; i<numbers.length(); i++){
-            dfs(numbers,"",i+1);
-        }
-        for(int x : list){
-            if(isPrimeNum(x)){
-                answer++;
-            }
-        }
-        return answer;
-    }
-    
-    public void dfs(String str,String temp,int length){
-        if(temp.length() == length){
-            int num = Integer.parseInt(temp);
-            if(!list.contains(num)){
-                System.out.println(num);
-                list.add(num);
-            }
-        }
-        for(int i=0; i<str.length(); i++){
-            if(!visited[i]){
-                visited[i] = true;
-                temp += str.charAt(i);
-                dfs(str,temp,length);
-                visited[i] = false;
-                temp = temp.substring(0,temp.length()-1);
-            }
-        }
-    }
-    
-    public boolean isPrimeNum(int x){
-        int i=2;
-        if(x<2) return false;
+        recursive("",numbers);
         
-        while(i*i <= x){
-            if(x % i == 0){
+        int count = 0;
+        for(int num : numberSet){
+            if(isPrime(num)){
+                count++;
+            }
+        }
+        return count;
+    }
+    
+    public void recursive(String comb,String others){
+        if(!comb.equals("")){
+            numberSet.add(Integer.parseInt(comb));
+        }
+        for(int i=0; i<others.length(); i++){
+            recursive(comb + others.charAt(i),others.substring(0,i)+others.substring(i+1));
+        }
+    }
+    
+    public boolean isPrime(int n){
+        if(n < 2) return false;
+        for(int i=2; i<=Math.sqrt(n); i++){
+            if(n % i ==0){
                 return false;
             }
-            i++;
         }
         return true;
     }
