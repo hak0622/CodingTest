7b8a9dc07a930feebfacbbbc70b339ae80fba9d2
hak0622 <128469147+hak0622@users.noreply.github.com>
2026-02-20 23:09:05 +0900

    [level 3] Title: 없어진 기록 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
+++ 프로그래머스/3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
@@ -1,5 +1,4 @@
 select ANIMAL_ID, NAME
 from ANIMAL_OUTS
-where ANIMAL_ID not in (select ANIMAL_ID
-                     from ANIMAL_INS)
-order by ANIMAL_ID
+where ANIMAL_ID NOT IN (select ANIMAL_ID
+                        from ANIMAL_INS)
