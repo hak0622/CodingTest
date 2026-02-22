c596d15474b0c2d843c85d75f9ff657232c544b2
hak0622 <128469147+hak0622@users.noreply.github.com>
2026-02-22 16:42:10 +0900

    [level 2] Title: 주식가격, Time: 23.01 ms, Memory: 69.6 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -3,14 +3,13 @@ class Solution {
         int[] answer = new int[prices.length];
         
         for(int i=0; i<prices.length; i++){
-            int count = 0;
             for(int j=i+1; j<prices.length; j++){
-                count++;
-                if(prices[j]<prices[i]){
+                answer[i]++;
+                
+                if(prices[i] > prices[j]){
                     break;
                 }
             }
-            answer[i]=count;
         }
         return answer;
     }
