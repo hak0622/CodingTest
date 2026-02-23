9bb6046a4159beb44b30b6578ffafeac6627822a
hak0622 <128469147+hak0622@users.noreply.github.com>
2026-02-23 22:29:41 +0900

    [level 2] Title: 기능개발, Time: 2.95 ms, Memory: 89.4 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -2,30 +2,28 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        int[]namugi = new int[progresses.length];
+        ArrayList<Integer>list = new ArrayList<>();
+        int day = 0;
+        Stack<Integer>st = new Stack<>();
         
         for(int i=0; i<progresses.length; i++){
-            namugi[i] = (int)Math.ceil((double)(100 - progresses[i]) / speeds[i]);
-        }
-        
-        List<Integer>list = new ArrayList<>();
-        
-        int day = namugi[0];
-        int count = 1;
-        
-        for(int i=1; i<namugi.length; i++){
-            if(namugi[i] <= day){
-                count++;
+            int count = 0;
+            
+            if((100 - progresses[i]) % speeds[i] == 0){
+                day = (100 - progresses[i]) / speeds[i];
             }else{
-                list.add(count);
-                count = 1;
-                day = namugi[i];
+                day = (100 - progresses[i]) / speeds[i] + 1;
+            }
+            
+            if(st.isEmpty() || st.peek() < day){
+                st.push(day);
+                list.add(1);
+            }else{
+                int index = list.size()-1;
+                list.set(index,list.get(index)+1);
             }
         }
-        list.add(count);
         
         return list.stream().mapToInt(Integer::intValue).toArray();
     }
 }
-
-
