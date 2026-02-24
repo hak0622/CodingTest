99e49f2a85f47fc8ed82ce800be3bb92e318d93c
hak0622 <128469147+hak0622@users.noreply.github.com>
2026-02-24 14:39:17 +0900

    [level 2] Title: 귤 고르기, Time: 52.27 ms, Memory: 110 MB -BaekjoonHub

---
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int k, int[] tangerine) {
+        int answer = 0;
+        HashMap<Integer,Integer>map = new HashMap<>();
+        
+        for(int i=0; i<tangerine.length; i++){
+            map.put(tangerine[i],map.getOrDefault(tangerine[i],0) + 1);
+        }
+        
+        List<Integer>list = new ArrayList<>(map.values());
+        list.sort(Collections.reverseOrder());
+        
+        for(int count : list){
+            k = k - count;
+            answer++;
+            
+            if(k <= 0){
+                break;
+            }
+        }
+        
+        return answer;
+    }
+}
