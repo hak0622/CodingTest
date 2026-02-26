ee5b763ff0075eb7d65bfac96ebbdf3bdb3d9555
hak0622 <128469147+hak0622@users.noreply.github.com>
2026-02-26 16:20:47 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 7.47 ms, Memory: 56.6 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -5,45 +5,36 @@ class Solution {
         int n = maps.length;
         int m = maps[0].length;
         
-        int[]dx = {1,-1,0,0};
-        int[]dy = {0,0,1,-1};
+        int[]dx = {-1,1,0,0};
+        int[]dy = {0,0,-1,1};
         
-        int[][]dist = new int[n][m];
+        boolean[][]visited = new boolean[n][m];
+        Queue<int[]>q = new LinkedList<>();
         
-        Queue<int[]> q = new LinkedList<>();
-        
-        if(maps[0][0] == 0) return -1;
-        
-        q.add(new int[]{0,0});
-        dist[0][0] = 1;
+        q.offer(new int[]{0,0});
+        visited[0][0] = true;
         
         while(!q.isEmpty()){
             int[]cur = q.poll();
             int x = cur[0];
             int y = cur[1];
             
-            if(x == n-1 && y == m-1){
-                return dist[x][y];
-            }
-            
             for(int d=0; d<4; d++){
                 int nx = x + dx[d];
                 int ny = y + dy[d];
                 
-                if(nx < 0 || nx >=n || ny < 0 || ny >= m){
-                    continue;
+                if(nx >= 0 && nx < n && ny >= 0 && ny < m){
+                    if(maps[nx][ny] == 1 && !visited[nx][ny]){
+                        visited[nx][ny] = true;
+                        maps[nx][ny] = maps[x][y] + 1;
+                        q.offer(new int[]{nx,ny});
+                    }
                 }
-                if(maps[nx][ny] == 0){
-                    continue;
-                }
-                if(dist[nx][ny] != 0){
-                    continue;
-                }
-                
-                dist[nx][ny] = dist[x][y] + 1;
-                q.add(new int[]{nx,ny});
             }
         }
-        return -1;
+        
+        int answer = maps[n-1][m-1];
+        
+        return (answer == 1) ? -1 : answer;
     }
 }
