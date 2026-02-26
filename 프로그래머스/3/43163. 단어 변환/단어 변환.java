9363a49a067a7a8d236cd443a93472aff4f4e7e5
hak0622 <128469147+hak0622@users.noreply.github.com>
2026-02-26 17:18:29 +0900

    [level 3] Title: 단어 변환, Time: 0.48 ms, Memory: 81.8 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -3,31 +3,33 @@ import java.util.*;
 class Solution {
     public int solution(String begin, String target, String[] words) {
         boolean[]visited = new boolean[words.length];
-        Queue<String>q = new LinkedList<>();
-        Queue<Integer>depthQ = new LinkedList<>();
         
-        q.add(begin);
-        depthQ.offer(0);
+        Queue<String>q = new LinkedList<>();
+        Queue<Integer>steps = new LinkedList<>();
+        
+        q.offer(begin);
+        steps.offer(0);
         
         while(!q.isEmpty()){
             String cur = q.poll();
-            int depth = depthQ.poll();
+            int step = steps.poll();
             
-            if(cur.equals(target)) return depth;
+            if(cur.equals(target)) return step;
             
             for(int i=0; i<words.length; i++){
-                if(!visited[i] && diffOne(cur,words[i])){
+                if(!visited[i] && move(cur,words[i])){
                     visited[i] = true;
-                    q.add(words[i]);
-                    depthQ.add(depth + 1);
+                    q.offer(words[i]);
+                    steps.offer(step + 1);
                 }
             }
         }
         return 0;
     }
     
-    boolean diffOne(String a, String b){
+    public boolean move(String a, String b){
         int diff = 0;
+        
         for(int i=0; i<a.length(); i++){
             if(a.charAt(i) != b.charAt(i)){
                 diff++;
