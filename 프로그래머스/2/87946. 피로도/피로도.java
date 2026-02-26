3306e9c0a46e5e09e4b77b966b0c583710d490b7
hak0622 <128469147+hak0622@users.noreply.github.com>
2026-02-26 15:08:47 +0900

    [level 2] Title: 피로도, Time: 3.60 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,22 +1,22 @@
 class Solution {
     int answer = 0;
+    boolean[]visited;
     
     public int solution(int k, int[][] dungeons) {
-        boolean[]visited = new boolean[dungeons.length];
-        dfs(k,dungeons,visited,0);
+        visited = new boolean[dungeons.length];
+        
+        dfs(k,dungeons,0);
+        
         return answer;
     }
     
-    void dfs(int k,int[][] dungeons,boolean[]visited,int count){
-        answer = Math.max(answer, count);
+    void dfs(int k, int[][]dungeons, int count){
+        answer = Math.max(answer,count);
         
         for(int i=0; i<dungeons.length; i++){
-            int need = dungeons[i][0];
-            int cost = dungeons[i][1];
-            
-            if(!visited[i] && k >= need){
+            if(!visited[i] && k >= dungeons[i][0]){
                 visited[i] = true;
-                dfs(k - cost, dungeons, visited, count +1);
+                dfs(k - dungeons[i][1], dungeons, count + 1);
                 visited[i] = false;
             }
         }
