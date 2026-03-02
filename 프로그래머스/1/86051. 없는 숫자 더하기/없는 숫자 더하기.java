b656692d01f7499b5664ee623ee7209064ca91ec
hak0622 <128469147+hak0622@users.noreply.github.com>
2026-03-02 19:55:38 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.04 ms, Memory: 87.9 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -1,15 +1,15 @@
 class Solution {
     public int solution(int[] numbers) {
         int answer = 0;
-        int[]num = new int[10];
+        int[] n = new int[10];
         
         for(int i=0; i<numbers.length; i++){
-            num[numbers[i]] = 1;
+            n[numbers[i]] = 1;
         }
         
-        for(int i=0; i<num.length; i++){
-            if(num[i] == 0){
-                answer = answer + i;
+        for(int i=0; i<n.length; i++){
+            if(n[i] == 0){
+                answer += i;
             }
         }
         return answer;
