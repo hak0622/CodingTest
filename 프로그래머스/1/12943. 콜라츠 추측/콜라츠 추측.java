e46f7c7b253a9a398dd852931bbac28d60e83ff9
hak0622 <128469147+hak0622@users.noreply.github.com>
2026-03-02 22:38:06 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.06 ms, Memory: 80.5 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -1,17 +1,19 @@
 class Solution {
-    public int solution(long num) {
+    public int solution(int num) {
+        long n = (long)num;
         int answer = 0;
-        while(num>1){
-            if(num%2==0){
-                num=num/2;
+        
+        while(n != 1){
+            if(n % 2 == 0){
+                n = n / 2;
             }else{
-                num=num*3+1;
+                n = n * 3 + 1;
             }
             answer++;
+            
+            if(answer >= 500) return -1;
         }
-        if(answer>=500){
-            answer = -1;
-        }
+        
         return answer;
     }
 }
