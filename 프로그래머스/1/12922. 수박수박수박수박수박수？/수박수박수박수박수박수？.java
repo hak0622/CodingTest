30d84d8211c52f7d64dd4116b16b6d85c906bb71
hak0622 <128469147+hak0622@users.noreply.github.com>
2026-03-02 23:18:38 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.94 ms, Memory: 91.8 MB -BaekjoonHub

--- 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -3,10 +3,10 @@ class Solution {
         StringBuilder sb = new StringBuilder();
         
         for(int i=1; i<=n; i++){
-            if(i%2 != 0){
-                sb.append("수");
-            }else{
+            if(i % 2 == 0){
                 sb.append("박");
+            }else{
+                sb.append("수");
             }
         }
         return sb.toString();
