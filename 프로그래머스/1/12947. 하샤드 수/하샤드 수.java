8dcc30cf7532b4304526ca22bd94957704254f27
hak0622 <128469147+hak0622@users.noreply.github.com>
2026-03-02 18:42:55 +0900

    [level 1] Title: 하샤드 수, Time: 7.63 ms, Memory: 89.3 MB -BaekjoonHub

--- 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
@@ -1,17 +1,14 @@
 class Solution {
     public boolean solution(int x) {
+        boolean answer = true;
+        String s = "" + x;
         int sum = 0;
-        int temp = x;
         
-        while(temp > 0){
-            sum = sum + temp % 10;
-            temp = temp / 10;
+        for(int i=0; i<s.length(); i++){
+            char c = s.charAt(i);
+            sum = sum + (c - '0');
         }
         
-        if(x % sum == 0){
-            return true;
-        }else{
-            return false;
-        }
+        return x % sum == 0 ? true : false;
     }
 }
