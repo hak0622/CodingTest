d7230f44673d1adcb50fdf499609290dbbde7da5
hak0622 <128469147+hak0622@users.noreply.github.com>
2026-03-03 15:03:05 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 5.60 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
@@ -1,21 +1,25 @@
 class Solution {
     public int[] solution(String s) {
-        int zero = 0;
+        int sum = 0;
         int count = 0;
         
-        String str = s;
-        
-        while(!str.equals("1")){
-            count++;
+        while(s.length() != 1){            
+            int deleteOne = 0;
             
-            int beforeLength = str.length();
-            str = str.replace("0","");
+            for(int i=0; i<s.length(); i++){
+                char c = s.charAt(i);
+                
+                if(c == '0'){
+                    deleteOne++;
+                }
+            }
             
-            int afterLength = str.length();
-            zero = zero + (beforeLength - afterLength);
+            sum = sum + deleteOne;
+            count = count + 1;
             
-            str = Integer.toBinaryString(afterLength);
+            s = Integer.toString(s.length() - deleteOne , 2);            
         }
-        return new int[]{count,zero};
+        
+        return new int[]{count,sum};
     }
 }
