2cc02f1e458fb07e981f535a16e6ced3b75e3cda
hak0622 <128469147+hak0622@users.noreply.github.com>
2026-03-04 22:05:18 +0900

    [level 2] Title: 숫자의 표현, Time: 1.94 ms, Memory: 67.1 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
@@ -3,20 +3,19 @@ class Solution {
         int answer = 0;
         
         for(int i=1; i<=n; i++){
-            int num = i;
             int sum = 0;
             
-            while(sum < n){
-                sum = sum + num;
-                num = num + 1;
+            for(int j=i; j<=n; j++){
+                sum += j;
                 
                 if(sum == n){
                     answer++;
                     break;
+                }else if(sum > n){
+                    break;
                 }
             }
         }
-        
         return answer;
     }
 }
