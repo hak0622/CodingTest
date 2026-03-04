79bd1ec8952e28eafe8e959789814b4f25463905
hak0622 <128469147+hak0622@users.noreply.github.com>
2026-03-04 23:52:42 +0900

    [level 2] Title: 피보나치 수, Time: 2.28 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -1,18 +1,13 @@
 class Solution {
     public int solution(int n) {
-         if (n == 0) return 0;
-        if (n == 1) return 1;
-
-        int a = 0;
-        int b = 1;
-        int result = 0;
-
-        for (int i = 2; i <= n; i++) {
-            result = (a + b) % 1234567;
-            a = b;
-            b = result;
+        int[]fibo = new int[n+1];
+        
+        fibo[0] = 0;
+        fibo[1] = 1;
+        
+        for(int i=2; i<=n; i++){
+            fibo[i] = (fibo[i-1] + fibo[i-2]) % 1234567;
         }
-
-        return result;
+        return fibo[n];
     }
 }
