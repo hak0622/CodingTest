418bfd07e885208d76f17c6351c846671070c0cb
hak0622 <128469147+hak0622@users.noreply.github.com>
2026-03-04 22:26:12 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.06 ms, Memory: 53.8 MB -BaekjoonHub

--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.java
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.java
@@ -1,37 +1,36 @@
 class Solution {
     public int solution(int n) {
-        int curOne = 0;
         int answer = 0;
+        int count1 = 0;
         
-        String curN = Integer.toString(n,2);
+        String s1 = Integer.toString(n,2);
         
-        for(int i=0; i < curN.length(); i++){
-            char c = curN.charAt(i);
+        for(int i=0; i<s1.length(); i++){
+            char c = s1.charAt(i);
             
             if(c == '1'){
-                curOne++;
+                count1++;
             }
         }
-        
+
         while(true){
+            int count2 = 0;
             n = n + 1;
-            int nextOne = 0;
-            String nextN = Integer.toString(n,2);
+            String s2 = Integer.toString(n,2);
             
-            for(int i=0; i<nextN.length(); i++){
-                char c = nextN.charAt(i);
+            for(int i=0; i<s2.length(); i++){
+                char c = s2.charAt(i);
                 
                 if(c == '1'){
-                    nextOne++;
+                    count2++;
                 }
             }
             
-            if(curOne == nextOne){
+            if(count1 == count2){
                 answer = n;
                 break;
             }
         }
-        
         return answer;
     }
 }
