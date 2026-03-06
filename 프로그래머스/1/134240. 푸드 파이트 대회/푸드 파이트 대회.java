e90cc6ff58e45c421624ac09f4c7ed865c762dc7
hak0622 <128469147+hak0622@users.noreply.github.com>
2026-03-06 21:59:01 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 12.31 ms, Memory: 100 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -3,17 +3,24 @@ class Solution {
         StringBuilder sb = new StringBuilder();
         
         for(int i=1; i<food.length; i++){
-            int count = food[i] / 2;
-            
-            for(int j=0; j<count; j++){
-                sb.append(i);
+            if(i % 2 != 0){
+                int n1 = food[i] / 2;
+                
+                for(int j=0; j<n1; j++){
+                    sb.append(i);
+                }
+            }else{
+                int n2 = food[i] / 2;
+                
+                for(int k=0; k<n2; k++){
+                    sb.append(i);
+                }
             }
         }
-        String left = sb.toString();
         
-        sb.append('0');
-        sb.append(new StringBuilder(left).reverse());
+        String s1 = sb.toString();
+        String s2 = sb.reverse().toString();
         
-        return sb.toString();
+        return s1 + "0" + s2;
     }
 }
