eb941eb7c35c687181139a4f6635443726f422a5
hak0622 <128469147+hak0622@users.noreply.github.com>
2026-03-08 16:26:13 +0900

    [level 1] Title: 카드 뭉치, Time: 0.75 ms, Memory: 87.6 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
@@ -1,15 +1,18 @@
+import java.util.*;
+
 class Solution {
     public String solution(String[] cards1, String[] cards2, String[] goal) {
+        String answer = "";
         int index1 = 0;
         int index2 = 0;
         
         for(int i=0; i<goal.length; i++){
-            String word = goal[i];
-            
-            if(index1 < cards1.length && cards1[index1].equals(word)){
+            if(index1 < cards1.length && goal[i].equals(cards1[index1])){
                 index1++;
-            }else if(index2 < cards2.length && cards2[index2].equals(word)){
+                System.out.println(index1);
+            }else if(index2 < cards2.length && goal[i].equals(cards2[index2])){
                 index2++;
+                System.out.println(index2);
             }else{
                 return "No";
             }
