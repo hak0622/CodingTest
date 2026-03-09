c219610bc8f7a01ec62d2e179aa5f36fd953192a
hak0622 <128469147+hak0622@users.noreply.github.com>
2026-03-09 21:41:35 +0900

    [level 1] Title: [1차] 비밀지도, Time: 6.73 ms, Memory: 73.7 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -1,19 +1,40 @@
-import java.util.*;
-
 class Solution {
     public String[] solution(int n, int[] arr1, int[] arr2) {
         String[] answer = new String[n];
         
         for(int i=0; i<n; i++){
-            int merged = arr1[i] | arr2[i];
-            String bin = Integer.toBinaryString(merged);
+            StringBuilder sb = new StringBuilder();
+            String s1 = Integer.toString(arr1[i],2);
+            String s2 = Integer.toString(arr2[i],2);
             
-            while(bin.length() < n){
-                bin = "0" + bin;
+            if(s1.length() < n){
+                int count1 = n - s1.length();
+                for(int k1=0; k1<count1; k1++){
+                    s1 = "0" + s1;
+                }
             }
             
-            bin = bin.replace('1','#').replace('0',' ');
-            answer[i] = bin;
+            if(s2.length() < n){
+                int count2 = n - s2.length();
+                for(int k2=0; k2<count2; k2++){
+                    s2 = "0" + s2;
+                }
+            }
+            System.out.println("s1 = " + s1);
+            System.out.println("s2 = " + s2);
+            
+            for(int j=0; j<n; j++){
+                char c1 = s1.charAt(j);
+                char c2 = s2.charAt(j);
+                
+                if(c1 == '1' || c2 == '1'){
+                    sb.append("#");
+                }else{
+                    sb.append(" ");
+                }
+            }
+            answer[i] = sb.toString();
+            System.out.println("answer = " + answer[i]);
         }
         return answer;
     }
