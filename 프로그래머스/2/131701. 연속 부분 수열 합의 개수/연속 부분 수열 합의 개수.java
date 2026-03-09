f3039fb056b311e8e1f8c11725810643f9fad45a
hak0622 <128469147+hak0622@users.noreply.github.com>
2026-03-09 22:48:05 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 153.43 ms, Memory: 119 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
@@ -4,21 +4,20 @@ class Solution {
     public int solution(int[] elements) {
         Set<Integer>set = new HashSet<>();
         int n = elements.length;
-        
         int[]extend = new int[n*2];
         
-        for(int i=0; i<n; i++){
-            extend[i] = extend[i+n] = elements[i];
+        for(int i=0; i<n*2; i++){
+            extend[i] = elements[i % n];
         }
         
         for(int i=0; i<n; i++){
             int sum = 0;
-            for(int j=i; j<i+n; j++){
-                sum = sum + extend[j];
+            
+            for(int j=0; j<n; j++){
+                sum = sum + extend[i + j];
                 set.add(sum);
             }
         }
-        
         return set.size();
     }
 }
