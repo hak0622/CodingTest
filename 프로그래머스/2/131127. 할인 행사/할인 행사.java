d2bb424cac553bd05eba900a8a884aae828f81c5
hak0622 <128469147+hak0622@users.noreply.github.com>
2026-03-10 15:54:12 +0900

    [level 2] Title: 할인 행사, Time: 74.87 ms, Memory: 120 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -3,24 +3,25 @@ import java.util.*;
 class Solution {
     public int solution(String[] want, int[] number, String[] discount) {
         int answer = 0;
-        Map<String,Integer>map1 = new HashMap<>();
+        Map<String,Integer>wantMap = new HashMap<>();
         
         for(int i=0; i<want.length; i++){
-            map1.put(want[i],number[i]);
+            wantMap.put(want[i],number[i]);
         }
         
-        
-        for(int i=0; i<=discount.length-10; i++){
-            Map<String,Integer>map2 = new HashMap<>();
+        for(int i=0; i<=discount.length - 10; i++){
+            Map<String,Integer>discountMap = new HashMap<>();
             
             for(int j=i; j<i+10; j++){
-                map2.put(discount[j],map2.getOrDefault(discount[j],0)+1);
+                String item = discount[j];
+                discountMap.put(item,discountMap.getOrDefault(item,0) + 1);
             }
             
-            if(map2.equals(map1)){
+            if(wantMap.equals(discountMap)){
                 answer++;
             }
         }
+        
         return answer;
     }
 }
