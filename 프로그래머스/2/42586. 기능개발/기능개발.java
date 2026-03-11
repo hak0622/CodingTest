291caa9b37f654e35800dff4dcd97a90158230b6
hak0622 <128469147+hak0622@users.noreply.github.com>
2026-03-11 18:06:46 +0900

    [level 2] Title: 기능개발, Time: 20.85 ms, Memory: 92.2 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -2,26 +2,32 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        ArrayList<Integer>list = new ArrayList<>();
-        int day = 0;
         Stack<Integer>st = new Stack<>();
+        List<Integer>list = new ArrayList<>();
+        int answer = 0;
         
         for(int i=0; i<progresses.length; i++){
-            int count = 0;
+            int n = (int)Math.ceil((100 - (double)progresses[i]) / (double)speeds[i]);
+            System.out.println("계산 후 = " + n);
             
-            if((100 - progresses[i]) % speeds[i] == 0){
-                day = (100 - progresses[i]) / speeds[i];
-            }else{
-                day = (100 - progresses[i]) / speeds[i] + 1;
+            if(st.isEmpty()){
+                answer++;
+                st.push(n);
+                System.out.println("answer = " + answer + " 비었었던 스택 = " + st);
+            }else if(st.peek() >= n){
+                answer++;
+                System.out.println("answer = " + answer + " peek 후 스택 = " + st);
+            }else if(st.peek() < n){
+                st.pop();
+                list.add(answer);
+                answer = 1;
+                st.push(n);
+                System.out.println("answer = " + answer + " pop 후 스택 = " + st);
             }
-            
-            if(st.isEmpty() || st.peek() < day){
-                st.push(day);
-                list.add(1);
-            }else{
-                int index = list.size()-1;
-                list.set(index,list.get(index)+1);
-            }
+        }
+        
+        if(answer != 0){
+            list.add(answer);
         }
         
         return list.stream().mapToInt(Integer::intValue).toArray();
