4e1acedb6bd62f78377d271966ace147cb9e076e
hak0622 <128469147+hak0622@users.noreply.github.com>
2026-03-12 15:38:43 +0900

    [level 2] Title: 롤케이크 자르기, Time: 266.78 ms, Memory: 172 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
@@ -1,31 +1,27 @@
+import java.util.*;
+
 class Solution {
     public int solution(int[] topping) {
         int answer = 0;
-        int maxTopping = 10001; //토핑 최대값 10000이라고 가정
-        int[] leftCount = new int[maxTopping]; //왼쪽 토핑 개수
-        int[] rightCount = new int[maxTopping]; //오른쪽 토핑 개수
-        int leftKind = 0;
-        int rightKind = 0;
         
-        //오른쪽 토핑 개수를 모두 세고 종류 수 센다
-        for(int i : topping){
-            if(rightCount[i] == 0){
-                rightKind++;
-            }
-            rightCount[i]++;
+        Set<Integer>set = new HashSet<>();
+        Map<Integer,Integer>map = new HashMap<>();
+        
+        for(int t : topping){
+            map.put(t,map.getOrDefault(t,0) + 1);
         }
-        //하나씩 왼쪽으로 옮기면서 비교
-        for(int i : topping){
-            if(leftCount[i] == 0){
-                leftKind++;
-            }
-            leftCount[i]++;
+        
+        for(int i=0; i<topping.length; i++){
+            int cur = topping[i];
             
-            rightCount[i]--;
-            if(rightCount[i] == 0){
-                rightKind--;
+            set.add(cur);
+            map.put(cur,map.get(cur) - 1);
+            
+            if(map.get(cur) == 0){
+                map.remove(cur);
             }
-            if(leftKind == rightKind){
+            
+            if(set.size() == map.size()){
                 answer++;
             }
         }
