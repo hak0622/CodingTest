004a2e9d034064188f05b6ec9d5ce98f0f7fe029
hak0622 <128469147+hak0622@users.noreply.github.com>
2026-03-13 17:07:59 +0900

    [level 1] Title: 덧칠하기, Time: 1.29 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
@@ -1,12 +1,12 @@
 class Solution {
     public int solution(int n, int m, int[] section) {
         int answer = 0;
-        int paint = 0;
+        int painted = 0;
         
-        for(int i=0; i<section.length; i++){
-            if(section[i] > paint){
+        for(int s : section){
+            if(s > painted){
                 answer++;
-                paint = section[i] + m - 1;
+                painted = s + m - 1;
             }
         }
         return answer;
