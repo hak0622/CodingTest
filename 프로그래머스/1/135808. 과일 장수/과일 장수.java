77409236c0d597b6890ea236cc29f6caa33adf35
hak0622 <128469147+hak0622@users.noreply.github.com>
2026-03-14 22:39:35 +0900

    [level 1] Title: 과일 장수, Time: 50.10 ms, Memory: 131 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.java
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.java
@@ -3,11 +3,20 @@ import java.util.*;
 class Solution {
     public int solution(int k, int m, int[] score) {
         int answer = 0;
+        int n = 0;
+        
         Arrays.sort(score);
         
-        for(int i=0; i<score.length / m; i++){
-            answer = answer + score[score.length - m * (i+1)] * m;
+        for(int i=score.length-1; i>=0; i--){
+            n++;
+            
+            if(n == m){
+                int sum = score[i] * m;
+                answer = answer + sum;
+                n = 0;
+            }
         }
+
         return answer;
     }
 }
