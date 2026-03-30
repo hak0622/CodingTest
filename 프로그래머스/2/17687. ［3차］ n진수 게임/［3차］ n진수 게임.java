85fd70ddc05b7705f8d595d6de6650941c6248ba
hak0622 <128469147+hak0622@users.noreply.github.com>
2026-03-30 17:22:19 +0900

    [level 2] Title: [3차] n진수 게임, Time: 15.72 ms, Memory: 93.1 MB -BaekjoonHub

--- 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
+++ 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
@@ -1,38 +1,19 @@
-import java.util.*;
-
 class Solution {
     public String solution(int n, int t, int m, int p) {
-        StringBuilder sb = new StringBuilder();
-        int num = 0;
-        
-        while(sb.length() < t * m){
-            String convert = Integer.toString(num,n).toUpperCase();
-            sb.append(convert);
-            num++;
-        }
-        
+        StringBuilder allNumbers = new StringBuilder();
         StringBuilder answer = new StringBuilder();
         
-        int turn = 1;
-        int answerCount = 0;
+        int currentNum = 0;
         
-        for(int i=0; i<sb.length(); i++){
-            char c = sb.charAt(i);
-            
-            if(turn == p){
-                answer.append(c);
-                answerCount++;
-            }
-            
-            if(answerCount == t){
-                break;
-            }
-            
-            turn++;
-            
-            if(turn > m){
-                turn = 1;
-            }
+        while (allNumbers.length() < t * m) {
+            String converted = Integer.toString(currentNum, n).toUpperCase();
+            allNumbers.append(converted);
+            currentNum++;
+        }
+        
+        for (int i = 0; i < t; i++) {
+            int targetIndex = (p - 1) + (i * m);
+            answer.append(allNumbers.charAt(targetIndex));
         }
         
         return answer.toString();
