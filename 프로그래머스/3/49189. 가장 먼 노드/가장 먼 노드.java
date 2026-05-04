b1172f113ee87b94800f1070d64c0dc1a1887503
hak0622 <128469147+hak0622@users.noreply.github.com>
2026-05-04 16:13:36 +0900

    [level 3] Title: 가장 먼 노드, Time: 48.85 ms, Memory: 120 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -1,52 +1,47 @@
 import java.util.*;
 
 class Solution {
-    ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
-    //인접 리스트에 그래프 저장
     public int solution(int n, int[][] edge) {
-        for(int i = 0 ; i <= n ; i++) graph.add(new ArrayList<>());
-		
-		for (int[] i : edge) {
-			int v = i[0];
-			int w = i[1];
-			graph.get(v).add(w);
-			graph.get(w).add(v);
-		}
-
-		boolean[] visit = new boolean[n + 1];
-        return bfs(graph, n, visit);
-    }
-    
-    public static int bfs(ArrayList<ArrayList<Integer>> graph, int n, boolean[] visit) {
-		Queue<int[]> q = new LinkedList<>();
-		int answer = 0;
-		
-		q.add(new int[] {1, 0});
-		visit[1] = true;
-		int maxDepth = 0;
-		
-		while(!q.isEmpty()) {
-			int[] arr = q.poll();
-			int v = arr[0];
-			int depth = arr[1];
-			
+        List<List<Integer>>graph = new ArrayList<>();
+        for(int i=0; i<=n; i++){
+            graph.add(i, new ArrayList<>());
+        }
+        
+        for(int[]e : edge){
+            int a = e[0];
+            int b = e[1];
             
-			if(maxDepth == depth) answer++; // 최대 길이 노드라면 answer++;
-			else if (maxDepth < depth) { // 더 긴 거리에 노드가 있다면 answer = 1, MaxDepth 갱신
-				maxDepth = depth;
-				answer = 1;
-			}
-
-			
-			for (int i = 0; i < graph.get(v).size(); i++) {
-				int w = graph.get(v).get(i); //인접 정점
-				if (!visit[w]) {
-					q.add(new int[] { w, depth + 1 });
-					visit[w] = true;
-				}
-			}
-		}
-
-		return answer;
-	}
+            graph.get(a).add(b);
+            graph.get(b).add(a);
+        }
+        
+        Queue<Integer>q = new LinkedList<>();
+        boolean[]visited = new boolean[n + 1];
+        int[]dist = new int[n + 1];
+        
+        q.add(1);
+        visited[1] = true;
+        
+        while(!q.isEmpty()){
+            int cur = q.poll();
+            
+            for(int next : graph.get(cur)){
+                if(!visited[next]){
+                    visited[next] = true;
+                    dist[next] = dist[cur] + 1;
+                    q.add(next);
+                }
+            }
+        }
+        int maxDist = 0;
+        for(int i=0; i<=n; i++){
+            maxDist = Math.max(maxDist,dist[i]);
+        }
+        int answer = 0;
+        for(int i=1; i<=n; i++){
+            if(dist[i] == maxDist) answer++;
+        }
+        
+        return answer;
+    }
 }
