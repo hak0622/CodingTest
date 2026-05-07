9f921548e2353765f6cd65a7fa1a0595009852b9
hak0622 <128469147+hak0622@users.noreply.github.com>
2026-05-07 13:14:04 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 4.45 ms, Memory: 57.9 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,8 +1,8 @@
 import java.util.*;
 
 class Solution {
-    int[]dx = {-1,1,0,0};
-    int[]dy = {0,0,-1,1};
+    int[]dx = {0,0,-1,1};
+    int[]dy = {1,-1,0,0};
     
     public int solution(int[][] maps) {
         int n = maps.length;
@@ -10,30 +10,31 @@ class Solution {
         boolean[][]visited = new boolean[n][m];
         Queue<int[]>q = new LinkedList<>();
         
-        q.add(new int[]{0,0,1});
+        q.add(new int[]{0,0});
         visited[0][0] = true;
+        int dist = 1;
         
         while(!q.isEmpty()){
-            int[]cur = q.poll();
-            int x = cur[0];
-            int y = cur[1];
-            int distance = cur[2];
+            int size = q.size();
             
-            if(x == n-1 && y == m-1){
-                return distance;
-            }
-            
-            for(int i=0; i<4; i++){
-                int nx = x + dx[i];
-                int ny = y + dy[i];
+            for(int i=0; i<size; i++){
+                int[]cur = q.poll();
+                int x = cur[0];
+                int y = cur[1];
                 
-                if(nx>=0 && nx < n && ny>=0 && ny < m){
-                    if(maps[nx][ny] == 1 && !visited[nx][ny]){
+                if(x == n - 1 && y == m - 1) return dist;
+                
+                for(int d=0; d<4; d++){
+                    int nx = x + dx[d];
+                    int ny = y + dy[d];
+                    
+                    if(nx >= 0 && nx < n && ny >= 0 && ny < m && maps[nx][ny] == 1 && !visited[nx][ny]){
                         visited[nx][ny] = true;
-                        q.add(new int[]{nx,ny,distance+1});
+                        q.add(new int[]{nx,ny});
                     }
                 }
             }
+            dist++;
         }
         return -1;
     }
