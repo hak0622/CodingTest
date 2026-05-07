c6debda369ee047a89add726262580555c41803f
hak0622 <128469147+hak0622@users.noreply.github.com>
2026-05-07 18:02:46 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 42.25 ms, Memory: 103 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,15 +1,14 @@
 import java.util.*;
-
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        HashMap<String,Integer> map = new HashMap<>();
+        HashMap<String,Integer>map = new HashMap<>();
         
         for(String p : participant){
             map.put(p, map.getOrDefault(p,0) + 1);
         }
         
         for(String c : completion){
-            map.put(c,map.get(c) -1);
+            map.put(c, map.get(c) - 1);
         }
         
         for(String key : map.keySet()){
