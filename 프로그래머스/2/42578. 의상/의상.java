76780be2e5628083f1354edb108e5364c6a2bd44
hak0622 <128469147+hak0622@users.noreply.github.com>
2026-05-11 01:21:42 +0900

    [level 2] Title: 의상, Time: 0.08 ms, Memory: 75.1 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -3,16 +3,16 @@ import java.util.*;
 class Solution {
     public int solution(String[][] clothes) {
         int answer = 1;
-        Map<String,Integer>map = new HashMap<>();
+        HashMap<String,Integer>map = new HashMap<>();
         
         for(int i=0; i<clothes.length; i++){
-            map.put(clothes[i][1],map.getOrDefault(clothes[i][1],0) + 1);
+            map.put(clothes[i][1], map.getOrDefault(clothes[i][1],0) + 1);
         }
         
         for(int count : map.values()){
             answer = answer * (count + 1);
         }
         
-        return answer-1;
+        return answer - 1;
     }
 }
