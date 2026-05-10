15996bbef1231f707a9d1997e7717b32688967dc
hak0622 <128469147+hak0622@users.noreply.github.com>
2026-05-11 01:52:45 +0900

    [level 3] Title: 베스트앨범, Time: 4.89 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
@@ -2,40 +2,37 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String[] genres, int[] plays) {
-        Map<String, Integer> totalMap = new HashMap<>();
-        Map<String, List<int[]>> musicMap = new HashMap<>();
-
-        for (int i = 0; i < genres.length; i++) {
-            totalMap.put(genres[i], totalMap.getOrDefault(genres[i], 0) + plays[i]);
-            
-            musicMap.putIfAbsent(genres[i], new ArrayList<>());
-            musicMap.get(genres[i]).add(new int[]{i, plays[i]}); 
-        }
-
-        List<String> sortedGenres = new ArrayList<>(totalMap.keySet());
-        sortedGenres.sort((g1, g2) -> totalMap.get(g2) - totalMap.get(g1));
-
-        List<Integer> answerList = new ArrayList<>();
+        HashMap<String,Integer>map1 = new HashMap<>();
+        HashMap<String,List<int[]>>map2 = new HashMap<>();
         
-        for (String genre : sortedGenres) {
-            List<int[]> songs = musicMap.get(genre);
-            
-            songs.sort((a, b) -> {
-                if (a[1] == b[1]) return a[0] - b[0]; 
-                return b[1] - a[1]; 
-            });
-
-            answerList.add(songs.get(0)[0]); 
-            if (songs.size() > 1) {
-                answerList.add(songs.get(1)[0]);
+        for(int i=0; i<genres.length; i++){
+            map1.put(genres[i], map1.getOrDefault(genres[i],0) + plays[i]);
+        }
+        List<String>list1 = new ArrayList<>(map1.keySet());
+        list1.sort((a,b) -> map1.get(b) - map1.get(a));
+        
+        for(int i=0; i<genres.length; i++){
+            if(!map2.containsKey(genres[i])) {
+                map2.put(genres[i], new ArrayList<>());
             }
-        }
-
-        int[] answer = new int[answerList.size()];
-        for (int i = 0; i < answerList.size(); i++) {
-            answer[i] = answerList.get(i);
+            map2.get(genres[i]).add(new int[]{i, plays[i]});
         }
         
-        return answer;
+        List<Integer>answer = new ArrayList<>();
+        
+        for(String genre : list1){
+            List<int[]>songs = map2.get(genre);
+            
+            songs.sort((a,b) -> {
+                if(a[1] == b[1]) return a[0] - b[0];
+                return b[1] - a[1];
+            });
+            
+            answer.add(songs.get(0)[0]);
+            
+            if(songs.size() > 1) answer.add(songs.get(1)[0]);
+        }
+        
+        return answer.stream().mapToInt(i->i).toArray();
     }
 }
