870ea692b02f05e4cdeb78008aed53d77fd665cf
hak0622 <128469147+hak0622@users.noreply.github.com>
2026-05-10 14:15:21 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 1.19 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,53 +1,46 @@
 import java.util.*;
 
 class Solution {
+    boolean[]visited;
+    List<List<Integer>>graph = new ArrayList<>();
+    
     public int solution(int n, int[][] wires) {
-        int answer = Integer.MAX_VALUE;
-        List<List<Integer>>graph = new ArrayList<>();
+        int answer = n;
         
-        for(int i=0; i<n; i++){
-            graph.add(i,new ArrayList<>());
+        for(int i=0; i<=n; i++){
+            graph.add(new ArrayList<>());
         }
-        for(int[]edge : wires){
-            int a = edge[0]-1;
-            int b = edge[1]-1;
+        
+        for(int[]w : wires){
+            int a = w[0];
+            int b = w[1];
             
             graph.get(a).add(b);
             graph.get(b).add(a);
         }
-        for(int[]edge : wires){
-            int a = edge[0]-1;
-            int b = edge[1]-1;
+        
+        for(int[]w : wires){
+            int v1 = w[0];
+            int v2 = w[1];
             
-            graph.get(a).remove(Integer.valueOf(b));
-            graph.get(b).remove(Integer.valueOf(a));
+            visited = new boolean[n + 1];
             
-            int count = bfs(a,graph,n);
+            int count = dfs(v1,v2);
             int diff = Math.abs(count - (n - count));
-            answer = Math.min(answer,diff);
-            
-            graph.get(a).add(b);
-            graph.get(b).add(a);
+            answer= Math.min(answer, diff);
         }
         return answer;
     }
-    public int bfs(int start,List<List<Integer>>graph,int n){
-        boolean[]visited = new boolean[n];
-        Queue<Integer>q = new LinkedList<>();
-        q.add(start);
-        visited[start]=true;
-        int cnt = 1;
+    
+    public int dfs(int curr, int block){
+        visited[curr] = true;
+        int count = 1;
         
-        while(!q.isEmpty()){
-            int current = q.poll();
-            for(int next : graph.get(current)){
-                if(!visited[next]){
-                    q.add(next);
-                    visited[next]=true;
-                    cnt++;
-                }
+        for(int next : graph.get(curr)){
+            if(!visited[next] && next != block){
+                count = count + dfs(next, block);
             }
         }
-        return cnt;
+        return count;
     }
 }
