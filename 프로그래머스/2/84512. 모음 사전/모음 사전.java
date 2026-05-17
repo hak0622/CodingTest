43bf6a01934f8228f9eb128dffdb8569a0b81ae5
hak0622 <128469147+hak0622@users.noreply.github.com>
2026-05-17 16:09:37 +0900

    [level 2] Title: 모음 사전, Time: 9.96 ms, Memory: 63.9 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -1,31 +1,25 @@
 class Solution {
-    String[]vowels = {"A","E","I","O","U"};
+    char[]voels = {'A','E','I','O','U'};
     int count = 0;
     int answer = 0;
-    boolean found = false;
     
     public int solution(String word) {
         dfs("",word);
         return answer;
     }
     
-    public void dfs(String current,String word){
-        if(found) return;
-        
-        if(!current.equals("")){
-            count++;
-            
-            if(current.equals(word)){
-                answer = count;
-                found = true;
-                return;
-            }
+    public void dfs(String cur, String target){
+        if(cur.equals(target)){
+            answer = count;
+            return;
         }
         
-        if(current.length() == 5) return;
+        if(cur.length() >= 5) return;
         
-        for(int i=0; i<vowels.length; i++){
-            dfs(current + vowels[i], word);
+        for(int i=0; i<5; i++){
+            count++;
+            dfs(cur + voels[i], target);
+            if(answer != 0) return;
         }
     }
 }
