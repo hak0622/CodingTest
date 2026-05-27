b1638c969c9f368ae54e965e7c5d1183f8c00255
hak0622 <128469147+hak0622@users.noreply.github.com>
2026-05-27 14:09:52 +0900

    [level 2] Title: 기능개발, Time: 1.47 ms, Memory: 62.8 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -2,29 +2,27 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        int[]days = new int[progresses.length];
+        int[]days = new int[speeds.length];
         
         for(int i=0; i<days.length; i++){
-            if((100 - progresses[i]) % speeds[i] != 0) days[i] = (100 - progresses[i]) / speeds[i] + 1;
-            else days[i] = (100 - progresses[i]) / speeds[i];
+            days[i] = (int)Math.ceil((100.0 - progresses[i]) / speeds[i]);
         }
         
-        List<Integer>list = new ArrayList<>();
-        int day = days[0];
+        List<Integer>answer = new ArrayList<>();
+        int maxDay = days[0];
         int count = 1;
         
         for(int i=1; i<days.length; i++){
-            if(days[i] <= day) {
+            if(days[i] <= maxDay){
                 count++;
-            }
-            else{
-                list.add(count);
+            }else{
+                answer.add(count);
+                maxDay = days[i];
                 count = 1;
-                day = days[i];
             }
         }
-        list.add(count);
+        answer.add(count);
         
-        return list.stream().mapToInt(i->i).toArray();
+        return answer.stream().mapToInt(i->i).toArray();
     }
 }
