aa6d18da507273c69ca9a11560c7cd7af89879f0
hak0622 <128469147+hak0622@users.noreply.github.com>
2026-05-28 12:59:51 +0900

    [level 2] Title: 올바른 괄호, Time: 7.28 ms, Memory: 58 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -3,7 +3,6 @@ import java.util.*;
 class Solution {
     boolean solution(String s) {
         Stack<Character>st = new Stack<>();
-        boolean answer = true;
         
         for(int i=0; i<s.length(); i++){
             char c = s.charAt(i);
