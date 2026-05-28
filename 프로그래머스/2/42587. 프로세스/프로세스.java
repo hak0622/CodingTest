f5a3e5e4ec836ea710179ac126e099b12c0e1df8
hak0622 <128469147+hak0622@users.noreply.github.com>
2026-05-28 13:31:22 +0900

    [level 2] Title: 프로세스, Time: 1.39 ms, Memory: 60.3 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -2,28 +2,27 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] priorities, int location) {
+        PriorityQueue<Integer>pq = new PriorityQueue<>(Collections.reverseOrder());
         Queue<int[]>q = new LinkedList<>();
         int answer = 0;
         
         for(int i=0; i<priorities.length; i++){
-            q.add(new int[]{priorities[i],i});
+            q.add(new int[]{i,priorities[i]});
+            pq.add(priorities[i]);
         }
         
         while(!q.isEmpty()){
             int[]cur = q.poll();
-            boolean high = false;
             
-            for(int[]p : q){
-                if(p[0] > cur[0]){
-                    high = true;
-                    break;
-                }
-            }
-            if(high){
-                q.add(cur);
-            }else{
+            if(cur[1] == pq.peek()){
+                pq.poll();
                 answer++;
-                if(cur[1] == location) return answer;
+                
+                if(cur[0] == location){
+                    return answer;
+                }
+            }else{
+                q.add(cur);
             }
         }
         return answer;
