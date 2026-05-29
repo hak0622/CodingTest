84a344849c87220e857115cde48da18005bf1550
hak0622 <128469147+hak0622@users.noreply.github.com>
2026-05-29 17:12:33 +0900

    [level 2] Title: 주식가격, Time: 30.45 ms, Memory: 78.8 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -1,18 +1,21 @@
+import java.util.*;
+
 class Solution {
     public int[] solution(int[] prices) {
-        int[] answer = new int[prices.length];
+        int[]answer = new int[prices.length];
+        Stack<Integer>st = new Stack<>();
         
         for(int i=0; i<prices.length; i++){
-            int cnt = 0;
-            
-            for(int j=i+1; j<prices.length; j++){
-                cnt++;
-                
-                if(prices[i] > prices[j]){
-                    break;
-                }
+            while(!st.isEmpty() && prices[st.peek()] > prices[i]){
+                int popIndex = st.pop();
+                answer[popIndex] = i - popIndex;
             }
-            answer[i] = cnt;
+            st.push(i);
+        }
+        
+        while(!st.isEmpty()){
+            int popIndex = st.pop();
+            answer[popIndex] = (prices.length - 1) - popIndex;
         }
         return answer;
     }
