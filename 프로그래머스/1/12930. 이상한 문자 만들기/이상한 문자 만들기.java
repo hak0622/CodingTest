35b39a5017ea4ba1dd9e599adfa40f98afbb854c
hak0622 <128469147+hak0622@users.noreply.github.com>
2026-06-09 13:27:45 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.06 ms, Memory: 75.5 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -1,24 +1,23 @@
 class Solution {
     public String solution(String s) {
-        String answer = "";
         StringBuilder sb = new StringBuilder();
-        int index = 0;
+        int count = 0;
         
         for(int i=0; i<s.length(); i++){
             char c = s.charAt(i);
             
             if(c == ' '){
-                index = 0;
+                count = 0;
                 sb.append(" ");
                 continue;
             }
             
-            if(index % 2 == 0){
+            if(count % 2 == 0) {
                 sb.append(Character.toUpperCase(c));
-                index++;
+                count++;
             }else{
                 sb.append(Character.toLowerCase(c));
-                index++;
+                count++;
             }
         }
         return sb.toString();
