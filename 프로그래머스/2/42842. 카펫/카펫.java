e8698472f4d3a0da38d4d9b7c2e4ebc13d9aab3b
hak0622 <128469147+hak0622@users.noreply.github.com>
2026-06-10 16:48:47 +0900

    [level 2] Title: 카펫, Time: 5.43 ms, Memory: 89.7 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,17 +1,20 @@
 class Solution {
     public int[] solution(int brown, int yellow) {
-        int[]answer = new int[2];
+        int[] answer = new int[2];
         int sum = brown + yellow;
+        int width = 1;
+        int height = 1;
         
-        for(int h=1; h*h<=sum; h++){
-            if(sum % h != 0) continue;
-            
-            int w = sum/h;
-            
-            if((w-2) * (h-2) == yellow){
-                answer[0] = w;
-                answer[1] = h;
-            }
+        for(int i=1; i<=sum; i++){
+            if(sum % i == 0){
+                width = sum / i;
+                height = sum / width;
+                
+                if(width >= height && (width - 2) * (height - 2) == yellow){
+                    answer[0] = width;
+                    answer[1] = height;
+                }   
+            }        
         }
         return answer;
     }
