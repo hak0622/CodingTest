2559e8bf5d49c74bb63a3afa8ec3f083b0d87f1a
hak0622 <128469147+hak0622@users.noreply.github.com>
2026-06-10 16:22:29 +0900

    [level 1] Title: 시저 암호, Time: 0.50 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.java
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.java
@@ -5,20 +5,15 @@ class Solution {
         for(int i=0; i<s.length(); i++){
             char c = s.charAt(i);
             
-            if(c == ' '){
-                sb.append(' ');
-                continue;
-            }
-            
-            if(c >= 'a' && c <= 'z'){
-                char shift1 = (char)((c - 'a' + n) % 26 + 'a');
-                sb.append(shift1);
-            }else if(c >= 'A' && c <= 'Z'){
-                char shift2 = (char)((c - 'A' + n) % 26 + 'A');
-                sb.append(shift2);
+            if(c == ' ') sb.append(" ");
+            else if(c>='A' && c<='Z'){
+                char move = (char)(((c-'A')+n) % 26 + 'A');
+                sb.append(move);
+            }else if(c>='a' && c<='z'){
+                char move = (char)(((c-'a')+n) % 26 + 'a');
+                sb.append(move);
             }
         }
-        
         return sb.toString();
     }
 }
