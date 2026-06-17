33a38ba85bec5d97b4bd5e309863b19d233ca341
hak0622 <128469147+hak0622@users.noreply.github.com>
2026-06-17 17:12:41 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.11 ms, Memory: 85.4 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -2,14 +2,14 @@ class Solution {
     public long solution(int n) {
         if (n == 1) return 1;
         
-        long[]dp = new long[n+1];
+        long[]jump = new long[n+1];
         
-        dp[1] = 1;
-        dp[2] = 2;
+        jump[1] = 1;
+        jump[2] = 2;
         
         for(int i=3; i<=n; i++){
-            dp[i] = (dp[i-1] + dp[i-2]) % 1234567;
+            jump[i] = (jump[i-1] + jump[i-2]) % 1234567;
         }
-        return dp[n];
+        return jump[n];
     }
 }
