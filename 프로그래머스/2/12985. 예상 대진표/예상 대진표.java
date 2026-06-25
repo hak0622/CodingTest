ceb595e8a1bccf34ca23db6bc9a1e779f4c2fe63
hak0622 <128469147+hak0622@users.noreply.github.com>
2026-06-25 18:22:19 +0900

    [level 2] Title: 예상 대진표, Time: 0.05 ms, Memory: 84 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
@@ -1,13 +1,12 @@
-class Solution{
-    public int solution(int n, int a, int b){
-        int answer = 0;
+class Solution {
+    public int solution(int n, int a, int b) {
+        int round = 0;
         
         while(a != b){
             a = (a + 1) / 2;
             b = (b + 1) / 2;
-            
-            answer++;
+            round++;
         }
-        return answer;
+        return round;
     }
 }
