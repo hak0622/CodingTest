21a368eb83fcf1c2a6cdd617d55ecfd02581e8fb
hak0622 <128469147+hak0622@users.noreply.github.com>
2026-07-04 17:34:03 +0900

    [level 1] Title: 기사단원의 무기, Time: 43.26 ms, Memory: 78.1 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.java
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.java
@@ -1,26 +1,29 @@
 class Solution {
     public int solution(int number, int limit, int power) {
-        int answer = 0;
+        int totalWeight = 0;
         
-        for(int i=1; i<=number; i++){
-            int count = 0;
+        for (int i = 1; i <= number; i++) {
+            int count = getDivisorCount(i);
             
-            for(int j=1; j*j<=i; j++){
-                if(i % j == 0){
-                    if(j * j == i){
-                        count = count + 1;
-                    }else{
-                        count = count + 2;
-                    }
-                }
-            }
-            
-            if(count > limit){
-                answer = answer + power;
-            }else{
-                answer = answer + count;
+            if (count > limit) {
+                totalWeight += power;
+            } else {
+                totalWeight += count;
             }
         }
-        return answer;
+        
+        return totalWeight;
+    }
+    
+    private int getDivisorCount(int n) {
+        int count = 0;
+        for (int i = 1; i * i <= n; i++) {
+            if (i * i == n) {
+                count++;
+            } else if (n % i == 0) {
+                count += 2;
+            }
+        }
+        return count;
     }
 }
