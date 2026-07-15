2c8f18e7194d84c8f8329d6f2195569efc039494
hak0622 <128469147+hak0622@users.noreply.github.com>
2026-07-15 14:23:10 +0900

    [level 2] Title: 더 맵게, Time: 680.26 ms, Memory: 139 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -1,24 +1,23 @@
-import java.util.PriorityQueue;
+import java.util.*;
 
 class Solution {
     public int solution(int[] scoville, int K) {
         int answer = 0;
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        PriorityQueue<Integer>pq = new PriorityQueue<>();
         
-        for (int s : scoville) {
-            pq.add(s);
+        for(int i=0; i<scoville.length; i++){
+            pq.add(scoville[i]);
         }
         
-        while (pq.peek() < K) {
-            if (pq.size() < 2) {
-                return -1;
-            }
+        while(pq.peek() < K){
+            if(pq.size() < 2) return -1;
             
-            int first = pq.poll();
-            int second = pq.poll();
+            int one = pq.poll();
+            int two = pq.poll();
             
-            int mixed = first + (second * 2);
-            pq.add(mixed);
+            int mix = one + (two * 2);
+            
+            pq.add(mix);
             answer++;
         }
         
