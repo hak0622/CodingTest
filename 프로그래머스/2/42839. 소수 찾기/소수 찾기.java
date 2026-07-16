90e164d99910ace28ed1d8127dc9aa7e1dc7b776
hak0622 <128469147+hak0622@users.noreply.github.com>
2026-07-16 17:27:44 +0900

    [level 2] Title: 소수 찾기, Time: 12.56 ms, Memory: 87.1 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -2,28 +2,28 @@ import java.util.*;
 
 class Solution {
     HashSet<Integer>set = new HashSet<>();
-    
+
     public int solution(String numbers) {
         boolean[]visited = new boolean[numbers.length()];
         int answer = 0;
         
-        dfs("", numbers, visited);
+        dfs("",numbers,visited);
         
         for(int n : set){
             if(isPrime(n)) answer++;
         }
+        
         return answer;
     }
     
-    
-    public void dfs(String cur, String numbers, boolean[]visited){
-        if(!cur.equals("")) set.add(Integer.parseInt(cur));
+    public void dfs(String s, String numbers, boolean[]visited){
+        if(!s.equals("")) set.add(Integer.parseInt(s));
         
         for(int i=0; i<numbers.length(); i++){
             if(visited[i]) continue;
             
             visited[i] = true;
-            dfs(cur + numbers.charAt(i) , numbers, visited);
+            dfs(s + numbers.charAt(i), numbers, visited);
             visited[i] = false;
         }
     }
@@ -31,7 +31,7 @@ class Solution {
     public boolean isPrime(int n){
         if(n < 2) return false;
         
-        for(int i=2; i*i<= n; i++){
+        for(int i=2; i*i<=n; i++){
             if(n % i == 0) return false;
         }
         return true;
