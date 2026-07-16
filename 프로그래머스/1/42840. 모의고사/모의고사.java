bbdf27c13274d001debc54b93c9f57c60fcca7d2
hak0622 <128469147+hak0622@users.noreply.github.com>
2026-07-16 17:15:20 +0900

    [level 1] Title: 모의고사, Time: 2.64 ms, Memory: 84 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -2,10 +2,10 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] answers) {
-        int[]answer = new int[3];
-        int[]supo1 = new int[]{1,2,3,4,5};
-        int[]supo2 = new int[]{2,1,2,3,2,4,2,5};
-        int[]supo3 = new int[]{3,3,1,1,2,2,4,4,5,5};
+        List<Integer>list = new ArrayList<>();
+        int[]supo1 = {1,2,3,4,5};
+        int[]supo2 = {2,1,2,3,2,4,2,5};
+        int[]supo3 = {3,3,1,1,2,2,4,4,5,5};
         int count1 = 0;
         int count2 = 0;
         int count3 = 0;
@@ -16,12 +16,10 @@ class Solution {
             if(answers[i] == supo3[i % supo3.length]) count3++;
         }
         
-        int maxCount = Math.max(count1, Math.max(count2, count3));
-        
-        List<Integer>list = new ArrayList<>();
-        if(maxCount == count1) list.add(1);
-        if(maxCount == count2) list.add(2);
-        if(maxCount == count3) list.add(3);
+        int max  = Math.max(count1, Math.max(count2,count3));
+        if(max == count1) list.add(1);
+        if(max == count2) list.add(2);
+        if(max == count3) list.add(3);
         
         return list.stream().mapToInt(i->i).toArray();
     }
