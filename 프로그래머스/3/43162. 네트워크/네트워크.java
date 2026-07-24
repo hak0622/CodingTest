f70bd58b51b5fc25adb629be444f4a6beaa17102
hak0622 <128469147+hak0622@users.noreply.github.com>
2026-07-24 18:11:04 +0900

    [level 3] Title: 네트워크, Time: 0.14 ms, Memory: 89 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -6,18 +6,18 @@ class Solution {
         for(int i=0; i<n; i++){
             if(!visited[i]){
                 answer++;
-                dfs(i,n, computers,visited);
+                dfs(i,n,computers,visited);
             }
         }
         return answer;
     }
     
-    public void dfs(int i,int n, int[][]computers,boolean[]visited){
+    public void dfs(int i, int n, int[][]computers, boolean[]visited){
         visited[i] = true;
         
         for(int j=0; j<n; j++){
             if(computers[i][j] == 1 && !visited[j]){
-                dfs(j,n,computers,visited);
+                dfs(j, n, computers, visited);
             }
         }
     }
