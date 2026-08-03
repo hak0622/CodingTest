278ea4100fd9fe7d0d9204f9f2d1f82ca1a3c452
hak0622 <128469147+hak0622@users.noreply.github.com>
2026-08-03 10:48:41 +0900

    [level 1] Title: 폰켓몬, Time: 1.82 ms, Memory: 89.3 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -4,10 +4,10 @@ class Solution {
     public int solution(int[] nums) {
         HashSet<Integer>set = new HashSet<>();
         
-        for(int n : nums){
-            set.add(n);
+        for(int i=0; i<nums.length; i++){
+            set.add(nums[i]);
         }
-                
-        return set.size() > nums.length / 2 ? nums.length / 2 : set.size();
+        
+        return Math.min(set.size(), nums.length / 2);
     }
 }
