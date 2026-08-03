4d333caa312ae51478a775830f7d71000c33eb51
hak0622 <128469147+hak0622@users.noreply.github.com>
2026-08-03 11:02:24 +0900

    [level 2] Title: 전화번호 목록, Time: 231.28 ms, Memory: 119 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -4,9 +4,10 @@ class Solution {
     public boolean solution(String[] phone_book) {
         Arrays.sort(phone_book);
         
-        for(int i=0; i<phone_book.length - 1; i++){
+        for(int i=0; i<phone_book.length-1; i++){
             if(phone_book[i+1].startsWith(phone_book[i])) return false;
         }
+        
         return true;
     }
 }
