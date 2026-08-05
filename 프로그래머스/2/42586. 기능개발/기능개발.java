8efe68e18f9d818ecbe27c426d24cad5675c28e2
hak0622 <128469147+hak0622@users.noreply.github.com>
2026-08-05 14:00:42 +0900

    [level 2] Title: 기능개발, Time: 2.78 ms, Memory: 81.5 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -2,18 +2,19 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        int[]days = new int[speeds.length];
         List<Integer>list = new ArrayList<>();
+        int[]days = new int[progresses.length];
         
         for(int i=0; i<days.length; i++){
-            days[i] = (int)Math.ceil((100.0 - progresses[i])/speeds[i]);
+            days[i] = (int)Math.ceil((100.0 - progresses[i]) / speeds[i]);
         }
         
         int max = days[0];
         int count = 1;
         
         for(int i=1; i<days.length; i++){
-            if(days[i] <= max){
+            
+            if(max >= days[i]){
                 count++;
             }else{
                 list.add(count);
@@ -23,7 +24,6 @@ class Solution {
         }
         
         list.add(count);
-                
         return list.stream().mapToInt(i->i).toArray();
     }
 }
