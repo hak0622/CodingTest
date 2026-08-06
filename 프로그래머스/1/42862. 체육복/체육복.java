f8068840ceffd8766abb5be6932cf4463a1b6ff4
hak0622 <128469147+hak0622@users.noreply.github.com>
2026-08-06 16:59:13 +0900

    [level 1] Title: 체육복, Time: 0.02 ms, Memory: 79.2 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -1,11 +1,11 @@
-import java.util.*;
-
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
         int answer = 0;
-        int[]clothes = new int[n+1];
-        Arrays.fill(clothes,1);
-        clothes[0] = 0;
+        int[]clothes = new int[n+2];
+        
+        for(int i=1; i<=n; i++){
+            clothes[i] = 1;
+        }
         
         for(int l : lost){
             clothes[l]--;
@@ -15,13 +15,13 @@ class Solution {
         }
         
         for(int i=1; i<=n; i++){
-            if(clothes[i] == 0){
-                if(i-1<=n && clothes[i-1] >= 2){
-                    clothes[i-1]--;
-                    clothes[i]++;
-                }else if(i+1<=n && clothes[i+1] >=2){
-                    clothes[i+1]--;
-                    clothes[i]++;
+            if(clothes[i] >= 2){
+                if(i-1 >= 1 && clothes[i-1] == 0){
+                    clothes[i]--;
+                    clothes[i-1]++;
+                }else if(i+1 <= n && clothes[i+1] == 0){
+                    clothes[i]--;
+                    clothes[i+1]++;
                 }
             }
         }
@@ -29,6 +29,7 @@ class Solution {
         for(int i=1; i<=n; i++){
             if(clothes[i] >= 1) answer++;
         }
+        
         return answer;
     }
 }
