faaba835ee10be90e9b1e0b2f7775e4237be09cb
hak0622 <128469147+hak0622@users.noreply.github.com>
2026-08-07 10:27:46 +0900

    [level 2] Title: 구명보트, Time: 9.28 ms, Memory: 58.9 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -2,14 +2,15 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] people, int limit) {
-        int answer = 0;
         Arrays.sort(people);
-        int front = 0;
+        int answer = 0;
+        int start = 0;
         int end = people.length - 1;
         
-        while(front <= end){
-            if(people[front] + people[end] <= limit) front++;
-            
+        while(start <= end){
+            if(people[start] + people[end] <= limit){
+                start++;
+            }
             answer++;
             end--;
         }
