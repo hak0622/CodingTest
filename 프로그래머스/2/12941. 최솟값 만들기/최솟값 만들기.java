2211f62cf6d34de7c0f9fd92746dec815595d958
hak0622 <128469147+hak0622@users.noreply.github.com>
2026-08-16 16:00:59 +0900

    [level 2] Title: 최솟값 만들기, Time: 6.36 ms, Memory: 91.3 MB -BaekjoonHub

--- 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.java
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.java
@@ -3,13 +3,12 @@ import java.util.*;
 class Solution{
     public int solution(int []A, int []B){
         int answer = 0;
-        int indexB = B.length - 1;
-        
+        int indexB = 0;
         Arrays.sort(A);
         Arrays.sort(B);
         
-        for(int i=0; i<A.length; i++){
-            answer = answer + A[i] * B[indexB - i];
+        for(int i=A.length-1; i>=0; i--){
+            answer = answer + A[i] * B[indexB++];
         }
         
         return answer;
