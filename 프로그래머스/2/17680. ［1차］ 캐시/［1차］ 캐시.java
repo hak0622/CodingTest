aeed3d62c035fcf8d41ffc9e8f3d43c502ae47e6
hak0622 <128469147+hak0622@users.noreply.github.com>
2026-08-24 13:53:41 +0900

    [level 2] Title: [1차] 캐시, Time: 23.47 ms, Memory: 117 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
@@ -4,22 +4,23 @@ class Solution {
     public int solution(int cacheSize, String[] cities) {
         int answer = 0;
         List<String>cache = new ArrayList<>();
-        
+                
         for(int i=0; i<cities.length; i++){
-            String s = cities[i].toLowerCase();
+            String city = cities[i].toUpperCase();
             
-            if(!cache.contains(s)){
-                cache.add(s);
-                answer = answer + 5;
-            }else {
-                cache.remove(s);
-                cache.add(s);
+            if(cache.contains(city)){
+                cache.remove(city);
+                cache.add(city);
                 answer = answer + 1;
+                continue;
+            }else{
+                cache.add(city);
+                answer = answer + 5;
+                
+                if(cache.size() > cacheSize){
+                    cache.remove(0);
+                }
             }
-            
-            if(cache.size() > cacheSize){
-                cache.remove(0);
-            }   
         }
         return answer;
     }
