af0a13d219dd91e97cd78fdeef8de0e1ff94d735
hak0622 <128469147+hak0622@users.noreply.github.com>
2026-08-24 14:05:07 +0900

    [level 2] Title: 피로도, Time: 1.69 ms, Memory: 86.4 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -7,13 +7,13 @@ class Solution {
         return answer;
     }
     
-    public void dfs(int k, int[][]dungeons, boolean[]visited, int count){
+    public void dfs(int k, int[][] dungeons,boolean[]visited,int count){
         if(answer < count) answer = count;
         
         for(int i=0; i<dungeons.length; i++){
             if(!visited[i] && k >= dungeons[i][0]){
                 visited[i] = true;
-                dfs(k - dungeons[i][1], dungeons, visited, count + 1);
+                dfs(k - dungeons[i][1], dungeons, visited, count+1);
                 visited[i] = false;
             }
         }
